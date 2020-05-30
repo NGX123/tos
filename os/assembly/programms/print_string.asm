402e45158fa2e0163649d67b0bbe09e24053bfb8
ngx123 <nikitja132@gmail.com>
2020-05-30 19:00:35 +0300

    os

--- os/assembly/programms/print_string.asm
+++ os/assembly/programms/print_string.asm
@@ -1,23 +1,7 @@
-;Boot sector that prints a message to the screen using ISR
-
-[org 0x7c00]
-
-mov bx, HELLO_MSG
-call print_string
-
-mov bx, GOODBYE_MSG
-call print_string
-
-jmp $
-
-
-
 print_string:
     pusha
     mov ah, 0x0e 
-    ; Move the value of bx - first letter of string into the al register
     
-
     print:
         ; Move the value of the address into al, first the address is the address of the first
         ;   letter(becuase it can not point to the whole string and only to the start) then it 
@@ -39,19 +23,11 @@ print_string:
         popa    ; Restore register values
         ret     ; Return to the caller of the functions
 
-; Hello string
-HELLO_MSG:
+
+TEST_MSG:
     db 'Hello, World', 0
 
-; Bye string
-GOODBYE_MSG:
-    db 'Goodbye', 0
 
-
-
-
-times 510-($-$$) db 0
-dw 0xaa55 
 
 
 
