391b5a0ef8bd8106ce3cba7ac01ad6f1a69a7fcc
ngx123 <nikitja132@gmail.com>
2020-07-01 11:24:09 +0300

    backup

--- os/gdt.h
+++
@@ -1,8 +0,0 @@
-// File: gdt.h
-// Description: contains the structure for the GLobal Descriptor Table
-
-
-struct gdt{
-    unsigned int address;
-    unsigned short size;
-} __attribute__((packed));
