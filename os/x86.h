1bd435e077f2c4c4f913c9334fa5e93c303a8d69
ngx123 <nikitja132@gmail.com>
2020-07-12 21:56:37 +0300

    commented lgdt

--- os/x86.h
+++ os/x86.h
@@ -15,14 +15,14 @@ static inline void outb(unsigned short port, unsigned char data){
     asm volatile("out %0,%1" : : "a" (data), "d" (port));
 }
 
-static inline void lgdt(struct gdt *p, int size)
-{
-  volatile uint16_t pd[3];
+// static inline void lgdt(struct gdt *p, int size)
+// {
+//   volatile uint16_t pd[3];
 
-  pd[0] = size-1;
-  pd[1] = (uint32_t)p;
-  pd[2] = (uint32_t)p >> 16;
+//   pd[0] = size-1;
+//   pd[1] = (uint32_t)p;
+//   pd[2] = (uint32_t)p >> 16;
 
-  asm volatile("lgdt (%0)" : : "r" (pd));
-}
+//   asm volatile("lgdt (%0)" : : "r" (pd));
+// }
 
