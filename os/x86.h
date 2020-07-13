f3388a67f4847e87a3e7b8311c94754e2c8c245f
ngx <npg2310@gmail.com>
2020-07-13 14:02:00 +0300

    remove lgdt

--- os/x86.h
+++ os/x86.h
@@ -1,6 +1,5 @@
 // File: x86.h
 // Description: includes functions and defines to use with the x86 CPUs
-// Problems:
 
 
 
@@ -15,14 +14,5 @@ static inline void outb(unsigned short port, unsigned char data){
     asm volatile("out %0,%1" : : "a" (data), "d" (port));
 }
 
-// static inline void lgdt(struct gdt *p, int size)
-// {
-//   volatile uint16_t pd[3];
 
-//   pd[0] = size-1;
-//   pd[1] = (uint32_t)p;
-//   pd[2] = (uint32_t)p >> 16;
-
-//   asm volatile("lgdt (%0)" : : "r" (pd));
-// }
 
