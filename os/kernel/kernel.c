52a29b9f2ab5cfa3ed7d67aff47390603b140cc5
ngx123 <nikitja132@gmail.com>
2020-07-17 12:59:12 +0300

    Changed to linking, added GDT

---
+++ os/kernel/kernel.c
@@ -0,0 +1,18 @@
+// File: kernel.c
+// Description: this is the main file which starts all other files
+// Problems:
+
+
+
+#include "../drivers/drivers.h"
+
+// Core function of the kernel that is called by bootloader
+void kernel_main(){
+    write_string(red, black, "Faro");
+    write_char(green, blue, 'A');
+    setFlat();
+    init_serial();
+    write_serial("faro");
+    
+}
+
