1bb6c6421acc6b7b614e2e945a21bde4a946b5c3
ngx123 <nikitja132@gmail.com>
2020-07-21 11:23:50 +0300

    added pic remapping

--- src/kernel/kernel.c
+++ src/kernel/kernel.c
@@ -8,14 +8,34 @@
 
 // Core function of the kernel that is called by bootloader
 void kernel_main(){
-    write_string(red, black, "Faro");
-    write_char(green, blue, 'A');
-    init_serial();
-    write_serial("faro");
     
-    outb(0x20, 0x20);
+    
+
+    prints(green, black, "Initialised: Display, ");
+    
+    init_serial();
+    write_serial("Serial test run");
+    prints(green, black, "Initialised: Serial, ");
+
     setFlat();
+    // Initialisation notice in assembly
+    
+    outb(0x20, 0x11);
+    outb(0xA0, 0x11); 
+    outb(0x21, 0x20); 
+    outb(0xA1, 40); 
+    outb(0x21, 0x04); 
+    outb(0xA1, 0x02); 
+    outb(0x21, 0x01);     
+    outb(0xA1, 0x01); 
+    outb(0x21, 0x0); 
+    outb(0xA1, 0x0);
     idt_init();
+    // Initialization notice in assembly 
+
+    
+    
+    
     
     
 }
