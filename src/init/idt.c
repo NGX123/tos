b69c9a846c3aa073b2570ee7e8d482062910b657
ngx123 <nikitja132@gmail.com>
2020-07-21 15:10:43 +0300

    added PIC remapping, changed the offset...

--- src/init/idt.c
+++ src/init/idt.c
@@ -1,43 +1,68 @@
 // File: idt.c
-// Description: c setup of interrupt descriptor table 
+// Description: c setup of interrupt descriptor table, the names of irqs must also be added here for usage. outb(0xA0, 0x20); needs to be added before out(0x20, 0x20); on interrupts 8+
 
-#define IDT_COUNT 1
+#define IDT_COUNT 256
+#include "../kernel/x86.h"
+extern void load_idt();
+extern int irq0();
 
-extern void load_idt(unsigned long);
-extern unsigned long isr0;
-
+// Defines a data structure for IDT
 struct idt_entry{
-    unsigned short offset1;
+    unsigned short offset_lowerbits;
     unsigned short selector;
 
     unsigned char zero;
     unsigned char flags;
-    unsigned short offset2;
+    unsigned short offset_higherbits;
 
 } __attribute__((packed));
 
+// Defines a pointer to the IDT data structure
 struct idt_pointer{
     unsigned short size;
     unsigned long address;
 } __attribute__((packed));
 
+
+
+
+// Initializations
 struct idt_entry IDT[IDT_COUNT];
 struct idt_pointer ip;
 
-
-
+// Initializes the interrupt descriptor table entries 
 void idt_init(){
+    // Remap the PIC
+    outb(0x20, 0x11);
+    outb(0xA0, 0x11); 
+    outb(0x21, 0x20); 
+    outb(0xA1, 40); 
+    outb(0x21, 0x04); 
+    outb(0xA1, 0x02); 
+    outb(0x21, 0x01);     
+    outb(0xA1, 0x01); 
+    outb(0x21, 0x0); 
+    outb(0xA1, 0x0);
 
-    IDT[0].offset1 = isr0 & 0xffff;
-    IDT[0].selector = 0x08;
-    IDT[0].zero = 0x0;
-    IDT[0].flags = 0xAE;
-    IDT[0].offset2 = (isr0 & 0xffff0000) >> 16;
-    
+
+
+
+    // Initialize the IDT entry with values
+	IDT[32].offset_lowerbits = (unsigned long)irq0 & 0xffff;
+	IDT[32].selector = 0x08; /* KERNEL_CODE_SEGMENT_OFFSET */
+	IDT[32].zero = 0;
+	IDT[32].flags = 0x8e; /* INTERRUPT_GATE */
+	IDT[32].offset_higherbits = ((unsigned long)irq0 & 0xffff0000) >> 16;
 
     ip.size = (sizeof(struct idt_entry) * IDT_COUNT) - 1;
     ip.address = (unsigned long)IDT;
 
-
     load_idt((unsigned long)&ip);
 }
+
+
+// C functions that handle interrupts
+void irq0_handler(void) {
+    outb(0x20, 0x20); //EOI
+}
+ 
--- src/init/idt.s
+++ src/init/idt.s
@@ -1,29 +1,18 @@
-asm_load_idt:
-        lidt [idt_descriptor]
-        sti
-        ret
-
-
-; Start address 
-idt_start:
-
-; First IRQ
-irq0:
-        dw      isr0           ; offset, split into two parts address of ISR entry point (0-15)
-        dw      0x0008          ; segment selector points to descriptor in GDT or LDT
-        db      0x0             ; set to 0 always 
-        db      10101110b     ; type and attr flags 
-        dw      0x0000          ; offset bits (16-31)
-
-
-idt_finish:
-
-; Descriptor(pointer) for loading into IDT register
-idt_descriptor:
-        dw idt_finish - idt_start - 1
-        dd idt_start
+; File: idt.s
+; Description: includes the assembly part of the irq handlers, to add new just changes the <NUM> in irq<NUM> for other number in all occurences
 
 
 
+; Expsoing irq addresses to be called
+global irq0
 
-; Service routine 0 - will be called if first interrupt is called 
+; External c functions for handing interrupts
+extern irq0_handler
+
+; Assembly functions hndlers
+irq0:  
+    pusha               
+    call irq0_handler
+    popa
+    iret
+
