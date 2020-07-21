54a71e5eb5904937f96f83ad356250bd8a70681c
ngx123 <nikitja132@gmail.com>
2020-07-21 15:11:21 +0300

    removed spaces

--- src/kernel/kernel.c
+++ src/kernel/kernel.c
@@ -8,10 +8,7 @@
 
 // Core function of the kernel that is called by bootloader
 void kernel_main(){
-    
-    
-
-    prints(green, black, "Initialised: Display, ");
+    prints(green, black, "Initialised: VGA, ");
     
     init_serial();
     write_serial("Serial test run");
@@ -20,16 +17,6 @@ void kernel_main(){
     setFlat();
     // Initialisation notice in assembly
     
-    outb(0x20, 0x11);
-    outb(0xA0, 0x11); 
-    outb(0x21, 0x20); 
-    outb(0xA1, 40); 
-    outb(0x21, 0x04); 
-    outb(0xA1, 0x02); 
-    outb(0x21, 0x01);     
-    outb(0xA1, 0x01); 
-    outb(0x21, 0x0); 
-    outb(0xA1, 0x0);
     idt_init();
     // Initialization notice in assembly 
 
