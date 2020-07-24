620f42c31616ba483daa713734aca05a835ec6af
ngx123 <nikitja132@gmail.com>
2020-07-24 12:08:38 +0300

    changed types

--- src/init/gdt.c
+++ src/init/gdt.c
@@ -2,23 +2,23 @@
 // Description: includes defenitions for loading the global descriptor table
 
 
-
-extern void load_gdt(unsigned long);
+#include <stdint.h>
+extern void load_gdt(uint64_t);
 
 // A gdt entry to be loaded using pointer
 struct gdt_entry{
-    unsigned short limit_low;
-    unsigned short base_low;
-    unsigned char base_middle;
-    unsigned char access;
-    unsigned char granularity;
-    unsigned char base_high;
+    uint16_t limit_low;
+    uint16_t base_low;
+    uint8_t base_middle;
+    uint8_t access;
+    uint8_t granularity;
+    uint8_t base_high;
 } __attribute__((packed));
 
 // Pointer to the GDT, The max bytes taken up by the GDT, minus 1
 struct gdt_ptr{
-    unsigned short limit;
-    unsigned long base;
+    uint16_t limit;
+    uint64_t base;
 } __attribute__((packed));
 
 // Our GDT, with 3 entries, and finally our special GDT pointer
@@ -49,7 +49,7 @@ void setFlat(){
     }
 
     gp.limit = (sizeof(struct gdt_entry) * 3) - 1;
-    gp.base = (unsigned long)gdt;
-    load_gdt((unsigned long)&gp);
+    gp.base = (uint64_t)gdt;
+    load_gdt((uint64_t)&gp);
 }
 
