75844eb9c78d299dff6940be5427bfa3dd97d0b2
ngx123 <nikitja132@gmail.com>
2020-08-04 14:35:58 +0300

    removed std*

--- src/kernel/kernel.h
+++ src/kernel/kernel.h
@@ -4,30 +4,6 @@
 
 
 #include <stdint.h>
-#define VGA 0xb8000
-
-char unsigned stdin;
-
-// Colors for fore and background of text in vga text mode
-enum VGA_COLOR {
-    black = 0x0,
-    blue = 0x1,
-    green = 0x2,
-    cyan = 0x3,
-    red = 0x4,
-    purple = 0x5,
-    brown = 0x6,
-    gray = 0x7,
-    dark_grey = 0x8,
-    light_blue = 0x9,
-    light_green = 0xA,
-    light_cyan = 0xB,
-    light_red = 0xC,
-    light_purple = 0xD,
-    yellow = 0xE,
-    white = 0xF
-};
-
 
 // External functions
 void printk(char* string);
