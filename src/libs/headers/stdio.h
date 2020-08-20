163d5c10473b5394768a3075c6bb93cf3e6a6119
NGX123 <nikitja132@gmail.com>
2020-08-20 22:48:23 +0300

    added standard input/output library

---
+++ src/libs/headers/stdio.h
@@ -0,0 +1,17 @@
+// File: stdio.h
+// Description: stdio input/output library header
+
+
+
+#ifndef STDIO_H
+#define STDIO_H
+
+// Prints a character to the screen
+extern void putchar(int chara);
+
+// Prints a string to the screen
+extern void puts(char* str);
+
+// Prints formatted output to the screen
+extern void printf(const char *fmt, ...);
+#endif
