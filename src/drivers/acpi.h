4341dc0d365b5b727922fd1a6e434d8659bb0f53
ngx <npg2310@gmail.com>
2020-08-22 19:41:21 +0300

    added normal search and fixed acpi

--- src/drivers/acpi.h
+++ src/drivers/acpi.h
@@ -6,6 +6,7 @@
 #include "stdio.h"
 #include "string.h"
 
+/// RSPD ///
 // Strucuture for the RSPD Descriptor in ACPI v2
 struct RSDPDescriptor {
     char Signature[8];
@@ -29,27 +30,49 @@ struct RSDPDescriptor *rsdp_tables[6];
 
 
 void findRSDPDescriptor(){
-    volatile char* start_serch = (volatile char*)0x00100000;	// Start of the memory
-    volatile char* end_search = (volatile char*)0xFFFFFFFF;     // End of the memory
+    volatile char* EBDApointer = (volatile char*)0x40E;
+    volatile char* EBDA = (volatile char*)*EBDApointer;
+    volatile char* EXTMEMstart = (volatile char*)0x000E0000;
+    volatile char* EXTMEMend = (volatile char*)0x000FFFFF;
+    printf("%p\n", EBDA);
     
     char array[9];      
-    long long i = 0;    // Counter for going through all memory
-    long long j = 0;    // Counter for going through the string in memory
+    int i = 0;          // Counter for going through all memory
+    int j = 0;          // Counter for going through the string in memory
     int k = 0;          // Counter for going through the string in array  
     int l = 0;          // Counter for storing the strcutres   
 
-    for (i = 0; &start_serch[i] != end_search; i++){
-        if (start_serch[i] == 'R'){
+    // Search EBDA
+    for (i = 0; i != 1023; i++){
+        if (EBDA[i] == 'R'){
             for (j = i, k = 0; j != (i+8); j++, k++)
-                array[k] = start_serch[j];
+                array[k] = EBDA[j];
             array[k] = 0;
 
             if (strcmp("RSD PTR ", array) == 0){
                 printf("\nACPI Device found\n");
-                rsdp_tables[l] = (struct RSDPDescriptor*)&start_serch[i];
-                printf("start_serch[i] - %p\n", &start_serch[i]);
+                rsdp_tables[l] = (struct RSDPDescriptor*)&EBDA[i];
+                printf("start_serch[i] - %p\n", &EBDA[i]);
                 printf("rsdp - %p\n", rsdp_tables[l]);
                 printf("%d", rsdp_tables[l++]->Revision);              
+            }       
+        }
+    }
+    i = 0; j = 0; k = 0;
+
+    // Search Extra Memory
+    for (int i = 0; &EXTMEMstart[i] != EXTMEMend; i++){
+        if (EXTMEMstart[i] == 'R'){
+            for (j = i, k = 0; j != (i+8); j++, k++)
+                array[k] = EXTMEMstart[j];
+            array[k] = 0;
+
+            if (strcmp("RSD PTR ", array) == 0){
+                printf("\nACPI Device found\n");
+                rsdp_tables[l] = (struct RSDPDescriptor*)&EXTMEMstart[i];
+                printf("EXTMEMstart[i] - %p\n", &EXTMEMstart[i]);
+                printf("Revision - %d\n", rsdp_tables[l]->Revision);     
+                printf("OEM ID - %s\n", rsdp_tables[l++]->OEMID);     
             }       
         }
     }
