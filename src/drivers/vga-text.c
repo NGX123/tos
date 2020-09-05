13d83eb77134a366bee63e712c44ecd8760accd1
NGX123 <nikitja132@gmail.com>
2020-09-05 10:02:17 +0300

    temporarily removed disable curosr function

--- src/drivers/vga-text.c
+++ src/drivers/vga-text.c
@@ -44,11 +44,11 @@ static void enable_cursor(uint8_t cursor_start, uint8_t cursor_end)
 }
 
 // Disable the cursor 
-static void disable_cursor()
-{
-	outb(0x3D4, 0x0A);
-	outb(0x3D5, 0x20);
-}
+// static void disable_cursor()
+// {
+// 	outb(0x3D4, 0x0A);
+// 	outb(0x3D5, 0x20);
+// }
 
 // Update the position of the cursor 
 static void update_cursor(int x, int y)
