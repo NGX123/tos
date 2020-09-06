4a303b886e19c4abdbe13776f6ec0289376224a5
NGX123 <nikitja132@gmail.com>
2020-09-06 21:20:38 +0300

    small changes

--- src/kernel/kernel.c
+++ src/kernel/kernel.c
@@ -3,36 +3,41 @@
 
 
 
-#include "driver.h"
-#include "x86.h"
+#include <stdint.h>
 #include "string.h"
 #include "stdio.h"
-#include "acpi.h"
 #include "libk.h"
 
+#include "vga.h"
+#include "x86.h"
+#include "acpi.h"
+
+
+extern void initSerial();
+extern void writeSerial(char* a);
 extern void  setFlat();
 extern void  idt_init();
-extern int keyboardInit(struct FADT* FADTstruct, uint8_t mode);
+extern int keyboardInit(uint8_t mode);
 
 // Core function of the kernel that is called by bootloader
 void kernel_main(){
     struct FADT* FADTstruct;
 
     // Turns the cursor on and sets it size
-    initScreen();
+    initScreen(1);
     printsys("Initialised: Screen\n");
-
-    // Sets the flat memory mode and initializes GDT
-    setFlat();
-    
-    // Initializes IDT
-    idt_init();
 
     // Writes data to set the ACPI mode
     if ((FADTstruct = (struct FADT*)ACPIcontrol(1)) != NULL){
         outb(FADTstruct->SMI_CommandPort, FADTstruct->AcpiEnable);  // Initialise the ACPI mode
         printsys("Initialised: ACPI\n");
     }
+
+    // Sets the flat memory mode and initializes GDT
+    setFlat();
+    
+    // Initializes IDT
+    idt_init();
         
     // Writes to serial port to configure it
     initSerial();
@@ -40,7 +45,7 @@ void kernel_main(){
     printsys("Initialised: Serial\n");
 
     // Initialise Keyboard
-    if (keyboardInit(FADTstruct, 1) != -1)
+    if (keyboardInit(1) != -1)
         printsys("Initialised: Keyboard\n");   
 }
 
