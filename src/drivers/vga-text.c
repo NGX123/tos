c6e04304ee3d1a09a70755847f5f6cabeba2f112
NGX123 <nikitja132@gmail.com>
2020-09-07 09:49:09 +0300

    changed include paths, added settings functionality to the initScreen, added option to change color of all text on the screen

--- src/drivers/vga-text.c
+++ src/drivers/vga-text.c
@@ -4,11 +4,12 @@
 
 
 /// Includes ///
-#include "x86.h"
-#include "driver.h"
+#include "drivers/x86.h"
+#include "drivers/vga.h"
 
 /// Defines ///
 #define VGA 0xb8000
+#define VGA_END 0xB8F9F
 #define VGA_WIDTH 80
 #define COLUMNS 80
 #define ROWS 25  
@@ -24,8 +25,8 @@ static volatile char* text_buffer = (volatile char*)VGA;
 static enum VGA_COLOR terminal_fg;
 static enum VGA_COLOR terminal_bg;
 static uint8_t color;
-static int cell; // Counts cells(2 bytes - char + color)
-static int byte; // Counts each byte in video memory
+static int cell;                                            // Counts cells(2 bytes - char + color)(last cell - 1999)
+static int byte;                                            // Counts each byte in video memory(last byte - 3999)
 static int x;
 static int y;
 
@@ -44,11 +45,11 @@ static void enable_cursor(uint8_t cursor_start, uint8_t cursor_end)
 }
 
 // Disable the cursor 
-// static void disable_cursor()
-// {
-// 	outb(0x3D4, 0x0A);
-// 	outb(0x3D5, 0x20);
-// }
+static void disable_cursor()
+{
+	outb(0x3D4, 0x0A);
+	outb(0x3D5, 0x20);
+}
 
 // Update the position of the cursor 
 static void update_cursor(int x, int y)
@@ -75,11 +76,15 @@ static void updatexy(){
 static void backspace(){
     uint8_t color = terminal_fg | terminal_bg << 4;
     
-    --byte;
-    --byte;
     text_buffer[byte] = BLANK;
+    text_buffer[byte+1] = BLANK;
 
     --cell;
+    byte = cell * 2;
+
+    text_buffer[byte] = BLANK;
+    text_buffer[byte+1] = BLANK;
+
     text_buffer[cell * 2 + 1] = color; 
     updatexy();
 }
@@ -104,7 +109,7 @@ static void arrows(const char direction){
         --byte;
         --cell;
     }
-    else if (text_buffer[cell * 2] != 0){
+    else if (direction == '>'){
         ++byte;
         ++byte;
         ++cell;
@@ -112,35 +117,69 @@ static void arrows(const char direction){
     updatexy();
 }
 
-// Clears the screen and moves the cursor to the start
-// static void clear(){
-//     volatile char* text_buffer = (char*)VGA;
-//     for (int i = 0; i <= 4000; i++){
-//         text_buffer[i] = BLANK;
-//     }
-//     byte = 0;
-//     cell = 0;
-//     updatexy();
-// }
+// Cleans the screen
+static void cleanScreen(char cleanOption){
+    int tmpCell;
+    int tmpByte;
+
+    // Clean colors and characters
+    if (cleanOption == 0){
+        for (tmpByte = 0; tmpByte < 4000; tmpByte++)
+            text_buffer[tmpByte] = BLANK;
+    }
+
+    // Clean characters
+    if (cleanOption == 1){
+        for (tmpCell = 0; tmpCell < 2000; tmpCell++)
+            text_buffer[tmpCell * 2] = BLANK;
+    }
+
+    // Clean color
+    if (cleanOption == 2){
+        for (tmpCell = 0; tmpCell < 2000; tmpCell++)
+            text_buffer[tmpCell * 2 + 1] = BLANK;
+    }
+}
 
 
 
 
 /// DRIVER INTERFACE ///
 // Initialises the screen
-void initScreen(){
+void initScreen(char cursor_status){
     terminal_fg = green;
     terminal_bg = black;
     color = terminal_fg | terminal_bg << 4;
+    
+    // Clear screen
+    cleanScreen(0);
 
-    enable_cursor(0, 15);
+    // Change the screen color
+    changeScreenColor(green, black);
+
+    // Change cursor status
+    if (cursor_status == 1)
+        enable_cursor(0, 15);
+    else 
+        disable_cursor();
 }
 
-// Changes screen color 
+// Changes color of all of the new text printed
 void changeColor(enum VGA_COLOR fg, enum VGA_COLOR bg) {
     terminal_fg = fg;
     terminal_bg = bg;
     color = terminal_fg | terminal_bg << 4;
+}
+
+// Changes the color of all text
+void changeScreenColor(enum VGA_COLOR fg, enum VGA_COLOR bg){
+    terminal_fg = fg;
+    terminal_bg = bg;
+    color = terminal_fg | terminal_bg << 4;
+
+    int tmpcell = 0;
+    while (tmpcell < 2000)
+        text_buffer[tmpcell++ * 2 + 1] = color; 
 }
 
 // Outputs a character to the screen
@@ -160,8 +199,10 @@ void printScreen(const uint8_t character){
         }  
     else if (character == LARROW)
         arrows('<');
-    else if (character == RARROW)
-        arrows('>');
+    else if (character == RARROW){
+        if (text_buffer[cell*2] != BLANK)
+            arrows('>');
+    }
     else if (character == '\b')
         backspace();
     else{
