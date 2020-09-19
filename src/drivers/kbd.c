e2377a51e824fe4b4c79df76e1ef7b6aece3fdf2
NGX123 <nikitja132@gmail.com>
2020-09-19 21:03:15 +0300

    moved out parts of keyboard interrupt handler into seperate functions

--- src/drivers/kbd.c
+++ src/drivers/kbd.c
@@ -18,6 +18,57 @@ static ring_buffer_t charRingBufferStruct;
 
 static uint8_t kbd_mode = 0;
 
+/// INTERRUPT HANDLER FUNCTIONS ///
+// Returns a status byte for all toggle and hold keys
+static uint8_t getButtonStatuses(uint32_t scancode, uint8_t buttonStatuses){
+    // Hold Key Statuses //
+    // Check ctrl 
+    if ((scancode == 0x1D      || scancode == 0x1DE0) && !(buttonStatuses & SHORTCUT_CTRL))
+        buttonStatuses ^= SHORTCUT_CTRL;
+    else if ((scancode == 0x9D || scancode == 0x9DE0) &&  (buttonStatuses & SHORTCUT_CTRL))
+        buttonStatuses ^= SHORTCUT_CTRL;
+
+    // Check alt 
+    if ((scancode == 0x38      || scancode == 0x38E0) && !(buttonStatuses & SHORTCUT_ALT))
+        buttonStatuses ^= SHORTCUT_ALT;
+    else if ((scancode == 0xB8 || scancode == 0xB8E0) &&  (buttonStatuses & SHORTCUT_ALT))
+        buttonStatuses ^= SHORTCUT_ALT;
+
+    // Check shift
+    if ((scancode == 0x2A      || scancode == 0x36)  && !(buttonStatuses & SHORTCUT_SHIFT))
+        buttonStatuses ^= (SHORTCUT_SHIFT | KEYBYTE_CAPS);
+    else if ((scancode == 0xAA || scancode == 0xB6)  &&  (buttonStatuses & SHORTCUT_SHIFT))
+        buttonStatuses ^= (SHORTCUT_SHIFT | KEYBYTE_CAPS);
+
+    // Check capslock
+    if (scancode == 0x3A      && !(buttonStatuses & SHORTCUT_CAPSLOCK))
+        buttonStatuses ^= (SHORTCUT_CAPSLOCK | KEYBYTE_CAPS);
+    else if (scancode == 0x3A &&  (buttonStatuses & SHORTCUT_CAPSLOCK))
+        buttonStatuses ^= (SHORTCUT_CAPSLOCK | KEYBYTE_CAPS);
+
+    return buttonStatuses;
+}
+
+// Gets the character from the scancode
+static uint8_t getCharacter(uint32_t scancode, uint8_t buttonStatuses){
+    // Initialize the character variable
+    uint8_t character = 0;
+
+    // Lowercase normal character
+    if (!(buttonStatuses & (SHORTCUT_CTRL | SHORTCUT_ALT | KEYBYTE_CAPS)) && !(scancode & 0x80))
+        character = scanset1[scancode];
+    
+    // Uppercase normal character
+    else if ((buttonStatuses & (KEYBYTE_CAPS)) && !(buttonStatuses & (SHORTCUT_CTRL | SHORTCUT_ALT)) && !(scancode & 0x80))
+        character = shiftmap[scancode];
+    
+    // Control shortcut
+    else if ((buttonStatuses & SHORTCUT_CTRL) && !(buttonStatuses & SHORTCUT_ALT))
+        character = ctlmap[scancode];
+
+    return character;
+}
+
 /// MODES ///
 // Keyboard standard mode interpretation function
 // Standard mode - arrows keys are straight passed to the display, all shortcuts are passed to system as interrupts(not made yet), normal keys are passed to the read buffer
@@ -73,45 +124,10 @@ void keyboard_handler(){
         scancode = scancode | (scancode_byte2 << 8);
     }
 
-    // Hold Key Statuses //
-    // Check ctrl 
-    if ((scancode == 0x1D      || scancode == 0x1DE0) && !(buttonStatuses & SHORTCUT_CTRL))
-        buttonStatuses ^= SHORTCUT_CTRL;
-    else if ((scancode == 0x9D || scancode == 0x9DE0) &&  (buttonStatuses & SHORTCUT_CTRL))
-        buttonStatuses ^= SHORTCUT_CTRL;
+    // Initialize the variables
+    buttonStatuses = getButtonStatuses(scancode, buttonStatuses);
+    character = getCharacter(scancode, buttonStatuses);
 
-    // Check alt 
-    if ((scancode == 0x38      || scancode == 0x38E0) && !(buttonStatuses & SHORTCUT_ALT))
-        buttonStatuses ^= SHORTCUT_ALT;
-    else if ((scancode == 0xB8 || scancode == 0xB8E0) &&  (buttonStatuses & SHORTCUT_ALT))
-        buttonStatuses ^= SHORTCUT_ALT;
-
-    // Check shift
-    if ((scancode == 0x2A      || scancode == 0x36)  && !(buttonStatuses & SHORTCUT_SHIFT))
-        buttonStatuses ^= (SHORTCUT_SHIFT | KEYBYTE_CAPS);
-    else if ((scancode == 0xAA || scancode == 0xB6)  &&  (buttonStatuses & SHORTCUT_SHIFT))
-        buttonStatuses ^= (SHORTCUT_SHIFT | KEYBYTE_CAPS);
-
-    // Check capslock
-    if (scancode == 0x3A      && !(buttonStatuses & SHORTCUT_CAPSLOCK))
-        buttonStatuses ^= (SHORTCUT_CAPSLOCK | KEYBYTE_CAPS);
-    else if (scancode == 0x3A &&  (buttonStatuses & SHORTCUT_CAPSLOCK))
-        buttonStatuses ^= (SHORTCUT_CAPSLOCK | KEYBYTE_CAPS);
-
-    // Initialize the character variable // 
-    character = 0;
-
-    // Lowercase normal character
-    if (!(buttonStatuses & (SHORTCUT_CTRL | SHORTCUT_ALT | KEYBYTE_CAPS)) && !(scancode & 0x80))
-        character = scanset1[scancode];
-    
-    // Uppercase normal character
-    else if ((buttonStatuses & (KEYBYTE_CAPS)) && !(buttonStatuses & (SHORTCUT_CTRL | SHORTCUT_ALT)) && !(scancode & 0x80))
-        character = shiftmap[scancode];
-    
-    // Control shortcut
-    else if ((buttonStatuses & SHORTCUT_CTRL) && !(buttonStatuses & SHORTCUT_ALT))
-        character = ctlmap[scancode];
 
     // Keyboard mode handling //
     if (kbd_mode == 0)
@@ -145,9 +161,6 @@ int keyboardInit(uint8_t mode){
 
     // Intialises the character buffer
     RingBufferInit(&charRingBufferStruct, BUFSIZE, charBuffer); 
-
-    // Initialises the packet buffer
-    //RingBufferInit(&packetRingBufferStruct, BUFSIZE, packetBuffer);
 
     // Enable first PS/2 port
     outb(KEYBOARD_COMMAND_PORT, 0xAE);                          // Enable 1st PS/2 port
