d5d9456d7c9d744ac13bbb5454708c4d43e71d78
NGX123 <nikitja132@gmail.com>
2020-09-26 11:00:41 +0300

    headers for utilities, one for sh added

---
+++ src/utils/headers/sh.h
@@ -0,0 +1,27 @@
+// File: sh.h
+// Description: includes defines for the sh.c
+
+
+
+// Handles errors
+void error_handler(int error_type);
+
+// Concatentates strings
+char* concat(const char *s1, const char *s2);
+
+// Formats the user input
+static void format_input(char* input);
+
+
+// Exits the shell
+int shell_exit(char **args);
+
+// Changes drectory of the current shell
+int shell_cd(char **args);
+
+// Executes the binary
+int shell_exec(char **args);
+
+
+// Main loop of the shell
+void shell_loop(void);
