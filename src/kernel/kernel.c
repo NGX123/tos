947e4a03af6ef67dab66936f61d015283a90a5bc
NGX123 <nikitja132@gmail.com>
2020-11-26 18:50:44 +0200

    moved includes, external and normal function declarations to kernel.h

--- src/kernel/kernel.c
+++ src/kernel/kernel.c
@@ -1,22 +1,7 @@
 // File: kernel.c
 // Description: this is the main file which starts all other files
 
-
-
-#include <stdint.h>
-#include "string.h"
-#include "stdio.h"
-#include "libk.h"
-
-#include "drivers/vga.h"
-#include "drivers/x86.h"
-#include "drivers/acpi.h"
-#include "drivers/kbd.h"
-
-extern void initSerial();
-extern void writeSerial(char* a);
-extern void  setFlat();
-extern void  idt_init();
+#include "kernel.h"
 
 // Core function of the kernel that is called by bootloader
 void kernel_main(){
@@ -34,10 +19,10 @@ void kernel_main(){
 
     // Sets the flat memory mode and initializes GDT
     setFlat();
-    
+
     // Initializes IDT
     idt_init();
-        
+
     // Writes to serial port to configure it
     initSerial();
     writeSerial("Serial test run");
@@ -47,4 +32,3 @@ void kernel_main(){
     if (keyboardInit(KEYBOARD_MODE_DISPLAY) != -1)
         printsys("Initialised: Keyboard\n");
 }
-
---
+++ src/kernel/kernel.h
@@ -0,0 +1,27 @@
+// File: kernel.h
+// Description: kernel code header
+
+#ifndef KERNEL_DEFINES_H
+#define KERNEL_DEFINES_H
+
+/// Includes
+#include <stdint.h>
+#include "string.h"
+#include "stdio.h"
+#include "libk.h"
+
+#include "drivers/vga.h"
+#include "drivers/x86.h"
+#include "drivers/acpi.h"
+#include "drivers/kbd.h"
+
+/// External declarations
+extern void initSerial();
+extern void writeSerial(char* a);
+extern void  setFlat();
+extern void  idt_init();
+
+/// Declarations
+// Core function of the kernel that is called by bootloader
+void kernel_main();
+#endif
