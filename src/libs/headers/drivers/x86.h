c9ef311a10d1b4718d3c1038bc4ee979ea5a06ba
NGX123 <nikitja132@gmail.com>
2020-11-26 07:57:53 +0200

    removed IDT and GDT function and structure declarations and moved them to separate

--- src/libs/headers/drivers/x86.h
+++ src/libs/headers/drivers/x86.h
@@ -8,44 +8,6 @@
 
 #include <stdint.h>
 
-/// GDT ///
-// A gdt entry to be loaded using pointer
-struct gdt_entry{
-    uint16_t limit_low;
-    uint16_t base_low;
-    uint8_t base_middle;
-    uint8_t access;
-    uint8_t granularity;
-    uint8_t base_high;
-} __attribute__((packed));
-
-// Pointer to the GDT, The max bytes taken up by the GDT, minus 1
-struct gdt_ptr{
-    uint16_t limit;
-    void* base;
-} __attribute__((packed));
-
-
-/// IDT ///
-// Defines a data structure for IDT
-struct idt_entry{
-    uint16_t offset_lowerbits;
-    uint16_t selector;
-
-    uint8_t zero;
-    uint8_t flags;
-    uint16_t offset_higherbits;
-
-} __attribute__((packed));
-
-// Defines a pointer to the IDT data structure
-struct idt_pointer{
-    uint16_t size;
-    void* address;
-} __attribute__((packed));
-
-
-
 /// PORT I/O ///
 // One byte port reading
 static inline uint8_t inb(uint16_t port){
@@ -74,4 +36,3 @@ static inline void outw(uint16_t port, uint16_t data)
   asm volatile("out %0,%1" : : "a" (data), "d" (port));
 }
 #endif
-
