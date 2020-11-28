b296921ccff31770b8941f4b83ea7078e6bd4c2d
NGX123 <nikitja132@gmail.com>
2020-11-28 19:55:55 +0200

    added a library including the types common types

---
+++ src/libs/headers/types.h
@@ -0,0 +1,13 @@
+// File: types.h
+// Description: includes type defenitions
+
+#ifndef TYPES_H
+#define TYPES_H
+
+#include <stddef.h>
+#include <stdint.h>
+#include <stdbool.h>
+
+typedef long ssize_t;
+#define NULL ((void *)0)
+#endif
