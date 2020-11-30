b4f140c63a61c1d13a4d161b2eb916797f737c82
NGX123 <nikitja132@gmail.com>
2020-11-30 17:25:06 +0200

    cut of the leftover spaces at the end of the lines

--- src/drivers/acpi.c
+++ src/drivers/acpi.c
@@ -6,7 +6,7 @@
 #include <stdint.h>
 #include "stdio.h"
 #include "string.h"
-#include "headers/acpi-defines.h"
+#include "headers/acpi.h"
 
 #include "drivers/acpi.h"
 
@@ -33,7 +33,7 @@ static int checksumRSDP(void* RSDPstruct){
 
     if (check == 0)
         return 0;
-        
+
     return -1;
 }
 
@@ -45,7 +45,7 @@ static int checksumHeaderACPI(void* voidPtr)
     char *checkPtr = (char*) ptr;
     int len = *(ptr + 1);
     char check = 0;
-    
+
     while (0 < len--){
         check += *checkPtr;
         checkPtr++;
@@ -61,12 +61,12 @@ static int checksumHeaderACPI(void* voidPtr)
 static void* findRSDPinEBDA(){
     volatile char* EBDApointer = (volatile char*)0x40E;
     volatile char* EBDA = (volatile char*)((int)*EBDApointer);
-    
+
     void* RSDPstruct;           // RSDP structure
-    char signature[9];          // Signature 
+    char signature[9];          // Signature
     int i = 0;                  // Counter for going through all memory
     int j = 0;                  // Counter for going through the string in memory
-    int k = 0;                  // Counter for going through the string in array  
+    int k = 0;                  // Counter for going through the string in array
 
     // Search EBDA
     for (i = 0; i != 1023; i++){
@@ -75,13 +75,13 @@ static void* findRSDPinEBDA(){
             for (j = i, k = 0; j != (i+8); j++, k++)
                 signature[k] = EBDA[j];
             signature[k] = 0;
-            
+
             // Case if signature found
             if (strcmp("RSD PTR ", signature) == 0){
                 RSDPstruct = (void*)&EBDA[i];
                 if (checksumRSDP(RSDPstruct) == 0)
-                    return RSDPstruct;       
-            }       
+                    return RSDPstruct;
+            }
         }
     }
     return NULL;
@@ -97,7 +97,7 @@ static void* findRSDPinEXTMEM(){
     int i;              // Counter for going through all memory
     int j;              // Counter for going through string in memory
     int k;              // Counter for going through string in array
-    
+
     for (i = 0; &EXTMEMstart[i] != EXTMEMend; i++){
         // Put signature into array
         if (EXTMEMstart[i] == 'R'){
@@ -109,8 +109,8 @@ static void* findRSDPinEXTMEM(){
             if (strcmp("RSD PTR ", signature) == 0){
                 RSDPstruct = (void*)&EXTMEMstart[i];
                 if (checksumRSDP(RSDPstruct) == 0)
-                    return RSDPstruct;   
-            }       
+                    return RSDPstruct;
+            }
         }
     }
     return NULL;
@@ -126,7 +126,7 @@ static void* findSDT(void* RSDPstruct, char* signature){
     // Case for ACPI v1
     if (((struct RSDP*)RSDPstruct)->Revision == 0){
         RSDTstruct = (struct RSDT*) (((struct RSDP*)RSDPstruct)->RsdtAddress);
-        
+
         if (checksumHeaderACPI((void*)RSDTstruct) != 0)
             return NULL;
 
@@ -135,7 +135,7 @@ static void* findSDT(void* RSDPstruct, char* signature){
     // Case for ACPI v2+
     else if (((struct RSDP*)RSDPstruct)->Revision > 0){
         RSDTstruct = (struct XSDT*) (((struct RSDP2*)RSDPstruct)->XsdtAddress);
-        
+
         if (checksumHeaderACPI((void*)RSDTstruct) != 0)
             return NULL;
 
@@ -150,7 +150,7 @@ static void* findSDT(void* RSDPstruct, char* signature){
             ACPISDTtmp = (struct ACPISDT*)(((struct RSDT*)RSDTstruct)->sdtptr+i);
         else if (((struct RSDP*)RSDPstruct)->Revision > 0)
             ACPISDTtmp = (struct ACPISDT*)(((struct XSDT*)RSDTstruct)->stdptr+i);
-    
+
         if (strncmp(ACPISDTtmp->Signature, signature, 4) == 0)
             if (checksumHeaderACPI((unsigned int*)ACPISDTtmp) == 0)
                 return (void*)ACPISDTtmp;
@@ -173,7 +173,7 @@ void* ACPIcontrol(int action){
             return NULL;
         else
             return FADTstruct;
-            
+
     }
     return NULL;
 }
