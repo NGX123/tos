6006fc7c87347d444a6fb013eaade95f6853cf2e
NGX123 <nikitja132@gmail.com>
2020-12-20 23:32:12 +0200

    added an infinite loop

--- src/uefi/main.c
+++ src/uefi/main.c
@@ -5,5 +5,9 @@ EFI_STATUS efi_main (EFI_HANDLE ImageHandle, EFI_SYSTEM_TABLE *SystemTable)
 {
   InitializeLib(ImageHandle, SystemTable);
   Print(L"Hello, world!\n");
+
+  while (1){
+  }
+
   return EFI_SUCCESS;
 }
