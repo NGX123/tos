9ac3bc18af567f564a08c0dbd25a95af613c50ac
NGX123 <nikitja132@gmail.com>
2020-12-25 12:45:51 +0200

    added gnu-efi code in addition to edk2 in main.c; created files for compilation with edk2

--- src/uefi/main.c
+++ src/uefi/main.c
@@ -4,5 +4,17 @@
 
 EFI_STATUS EFIAPI UefiMain (IN EFI_HANDLE ImageHandle, IN EFI_SYSTEM_TABLE *SystemTable){
   Print(L"Hello World \n");
+  while(1){}
   return EFI_SUCCESS;
 }
+
+// GNU-EFI
+// #include <efi.h>
+// #include <efilib.h>
+// EFI_STATUS EFIAPI efi_main (EFI_HANDLE ImageHandle, EFI_SYSTEM_TABLE *SystemTable) {
+//   InitializeLib(ImageHandle, SystemTable);
+//   Print(L"Hello, world!\n");
+
+//   while(1){}
+//   return EFI_SUCCESS;
+// }
