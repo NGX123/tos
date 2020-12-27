4e5651a0979d7a7c04b59183c266e1af72608a95
NGX123 <nikitja132@gmail.com>
2020-12-27 09:04:19 +0200

    added a macro definition checking to not change the code each time to compile with GNU-EFI and back to EDK2 then

--- src/uefi/main.c
+++ src/uefi/main.c
@@ -1,3 +1,6 @@
+#ifndef GNUEFI_BUILD
+
+// Code for compilation with EDK2 Toolkit
 #include <Uefi.h>
 #include <Library/UefiApplicationEntryPoint.h>
 #include <Library/UefiLib.h>
@@ -8,13 +11,17 @@ EFI_STATUS EFIAPI UefiMain (IN EFI_HANDLE ImageHandle, IN EFI_SYSTEM_TABLE *Syst
   return EFI_SUCCESS;
 }
 
-// GNU-EFI
-// #include <efi.h>
-// #include <efilib.h>
-// EFI_STATUS EFIAPI efi_main (EFI_HANDLE ImageHandle, EFI_SYSTEM_TABLE *SystemTable) {
-//   InitializeLib(ImageHandle, SystemTable);
-//   Print(L"Hello, world!\n");
+#else
 
-//   while(1){}
-//   return EFI_SUCCESS;
-// }
+// Code for compilation with GNU-EFI Toolkit
+#include <efi.h>
+#include <efilib.h>
+EFI_STATUS EFIAPI efi_main (EFI_HANDLE ImageHandle, EFI_SYSTEM_TABLE *SystemTable) {
+  InitializeLib(ImageHandle, SystemTable);
+  Print(L"Hello, world!\n");
+
+  while(1){}
+  return EFI_SUCCESS;
+}
+
+#endif
