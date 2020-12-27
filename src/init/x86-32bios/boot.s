c9d61067b0ccc3ac26cd0722a5a19f7105d27b05
NGX123 <nikitja132@gmail.com>
2020-12-27 09:45:52 +0200

    changed the incldue path

--- src/init/x86-32bios/boot.s
+++ src/init/x86-32bios/boot.s
@@ -14,11 +14,11 @@ CHECKSUM equ -(MAGIC + FLAGS)   ; checksum of above, to prove we are multiboot
 
 
 ;;; Multiboot(grub) section ;;;
-section .multiboot 
+section .multiboot
 
 ; Writes special data aligned to 3 bytes according to the multiboot standard for programm to be loaded by GRUB
 align 4
-    dd MAGIC 
+    dd MAGIC
     dd FLAGS
     dd CHECKSUM
 
@@ -26,27 +26,27 @@ align 4
 
 
 ;;; Stack Section ;;;
-section .bss 
+section .bss
 
 ; Stack requires to be 16 bytes aligned, then 16 kib are allocated for the stack
 align 16
 stack_bottom:
 ; Allocate 16 kilobytes
-resb 16384 
+resb 16384
 stack_top:
 
 
 
 
 ;;; Code section ;;;
-section .text 
+section .text
 
 ; Linker specifies _start as the entry to the kernel, bootloader jumps here once his work is done(loads us into 32-bit protected mode)
 global _start:function ; (_start.end - _start) ; add if something does not work
 _start:
-        ; Setup stack by loading memory address of the top of the stack into stack register 
+        ; Setup stack by loading memory address of the top of the stack into stack register
         mov esp, stack_top
-        
+
         ; Call external C function
         extern kernel_main
         call kernel_main
@@ -96,8 +96,4 @@ load_idt:
 
 
 ; IDT code
-%include "init/idt.s"
-
-
-
-
+%include "init/x86-32bios/idt.s"
