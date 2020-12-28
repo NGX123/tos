c128f3825f3d8d3708386e3286443b51fd1562a4
NGX123 <nikitja132@gmail.com>
2020-12-28 12:39:33 +0200

    platform independant core interrupt handling code

---
+++ src/kernel/headers/interrupts.h
@@ -0,0 +1,5 @@
+// File: interrupts.h
+// Description: defines for platform independant interrupt code
+
+// Signal handler function type
+typedef void (*signalhandler_t)(int);
---
+++ src/kernel/interrupts.c
@@ -0,0 +1,9 @@
+// File: interrupts.c
+// Description: is platform independant main part of interrupt handling code
+
+#include "headers/interrupts.h"
+
+// Change the function called by interrupt
+int signal(int signalnum, signalhandler_t handlerfunc){
+
+}
