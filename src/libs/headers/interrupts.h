63ffe8d9e71fcdd9512271abb8c6821b9654d36e
NGX123 <NGX123>
2020-12-31 21:35:01 +0200

    added more description comments

--- src/libs/headers/interrupts.h
+++ src/libs/headers/interrupts.h
@@ -8,7 +8,11 @@
 #define INTERRUPT_PRIORITY_KERNEL 2
 #define INTERRUPT_PRIORITY_USER 3
 
+
+// Function type of an interrupt handler that is called when certain interrupt occures
 typedef void (*interrupt_handler_t)(int);
+
+// Function type of the kernels interrupt interpreter function
 typedef int (*interrupt_interpreter_func_t)(int);
 
 
