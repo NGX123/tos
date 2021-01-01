4fe8f032e67e816590fe3c7bf609a346fcaffda0
NGX123 <NGX123>
2021-01-01 19:43:20 +0200

    made printsys look like systemd-init

--- src/libs/headers/libk.h
+++ src/libs/headers/libk.h
@@ -4,8 +4,11 @@
 #ifndef LIBK_H
 #define LIBK_H
 
+#define PRINTSYS_STATUS_SUCCESS 0
+#define PRINTSYS_STATUS_FAIL 1
+
 // Prints a system message with special formatting
-extern void printsys(char* string);
+extern void printsys(char* string, int status);
 
 // Print a message when gdt was initialised
 extern void display_gdt();
--- src/libs/libk.c
+++ src/libs/libk.c
@@ -1,20 +1,40 @@
 // File: libk.c
 // Description: includes special function that will be available only to the kernel
 
-
-
 #include "stdio.h"
+#include "drivers/vga.h"
 #include "libk.h"
 
-void printsys(char* string){
-    printf("[ INIT ] %s", string);
+void printsys(char* string, int status){
+    // Success Message
+    if (status == PRINTSYS_STATUS_SUCCESS){
+        changeColor(white, black, CHANGE_COLOR_NEXT);
+        printf("[ ");
+        changeColor(green, black, CHANGE_COLOR_NEXT);
+        printf("OK");
+        changeColor(white, black, CHANGE_COLOR_NEXT);
+        printf(" ]   ");
+    }
+    // Fail Message
+    else if (status == PRINTSYS_STATUS_FAIL){
+        changeColor(white, black, CHANGE_COLOR_NEXT);
+        printf("[ ");
+        changeColor(red, black, CHANGE_COLOR_NEXT);
+        printf("FAIL");
+        changeColor(white, black, CHANGE_COLOR_NEXT);
+        printf(" ] ");
+    }
+
+    // Message
+    changeColor(green, black, CHANGE_COLOR_NEXT);
+    printf("%s", string);
 }
 
 // Info prints ///
 void display_gdt(){
-    printsys("Initialized: GDT\n");
+    printsys("Initialized: GDT\n", PRINTSYS_STATUS_SUCCESS);
 }
 
 void display_idt(){
-    printsys("Initialised: IDT\n");
+    printsys("Initialised: IDT\n", PRINTSYS_STATUS_SUCCESS);
 }
