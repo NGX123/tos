5d6de02c6db8a0c1331b9257bf75f4e3195cceba
NGX123 <NGX123>
2021-01-01 21:04:01 +0200

    added a file with defines for platform specific functions

---
+++ src/libs/headers/platform.h
@@ -0,0 +1,9 @@
+// File: platform.h
+// Description: includes declaration for the platform specific functions
+
+#ifndef PLATFORM_H
+#define PLATFORM_H
+
+// Makes platform specific initializations
+void hardwarePlatformInit();
+#endif
