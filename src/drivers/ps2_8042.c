9cc4a8ec41b1eba2574b8762ec1aac1486e498f3
NGX123 <NGX123>
2021-01-03 18:16:26 +0200

    added initialization function

--- src/drivers/ps2_8042.c
+++ src/drivers/ps2_8042.c
@@ -2,3 +2,22 @@
 // Description: a device driver for the 8042 PS/2 controller
 
 #include "headers/ps2_8042.h"
+
+int ps2ControllerInit(){
+    // uint8_t config_byte;
+
+    // // Disable ports so they don't interrupt
+    // outb(CONTROLLER_COMMAND_PORT, DISABLE_PS2_PORT1);
+    // outb(CONTROLLER_COMMAND_PORT, DISABLE_PS2_PORT2);
+
+    // // Change the config byte
+    // outb(CONTROLLER_COMMAND_PORT, READ_CONFIG_BYTE);
+    // if (inb(CONTROLLER_STATUS_PORT) & STATUS_OUTPUT_BUFFER)
+    //     printf("%x", inb(CONTROLLER_DATA_PORT));
+
+    outb(CONTROLLER_COMMAND_PORT, TEST_PS2_PORT1);
+    if (inb(CONTROLLER_STATUS_PORT) & STATUS_OUTPUT_BUFFER)
+        printf("%d\n", inb(CONTROLLER_DATA_PORT));
+
+    return 0;
+}
