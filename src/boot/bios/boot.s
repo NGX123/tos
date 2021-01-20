2c062ee71625cfbb870bfb42cc730150668e1043
NGX123 <nikitja132@gmail.com>
2021-01-20 09:42:06 +0200

    fixed formatting

--- src/boot/bios/boot.s
+++ src/boot/bios/boot.s
@@ -8,11 +8,11 @@ global _start   ; If errors - _start:function
 extern kernel_main
 
 
-MBALIGN equ 1 << 0              ; Loaded modules page boundaries aligning flag
-MEMINFO equ 1 << 1              ; Memory map flag
-FLAGS   equ MBALIGN | MEMINFO   ; GRUB boot flags combination
-MAGIC   equ 0x1BADB002          ; Value for grub detection
-CHECKSUM equ -(MAGIC + FLAGS)   ; checksum of above, to prove we are multiboot
+MBALIGN         equ 1 << 0              ; Loaded modules page boundaries aligning flag
+MEMINFO         equ 1 << 1              ; Memory map flag
+FLAGS           equ MBALIGN | MEMINFO   ; GRUB boot flags combination
+MAGIC           equ 0x1BADB002          ; Value for grub detection
+CHECKSUM        equ -(MAGIC + FLAGS)   ; checksum of above, to prove we are multiboot
 
 
 section .multiboot              ; GRUB section, includes writing special data at start of binary for grub to see the OS
@@ -25,7 +25,7 @@ align 4
 section .bss
 align 16                        ; Aligning is required for stack
         stack_bottom:
-                resb 16384      ; Allocate 16 kilobytes
+                resb 16384      ; Allocate 16 kilobytes for stack
         stack_top:
 
 
