302d44e67a6fa1f2f4ee44a0c6ef3e750f38eca0
NGX123 <nikitja132@gmail.com>
2021-01-22 12:38:15 +0200

    added a mmap structure to fix the field mixing

--- src/include/kernel/multiboot.h
+++ src/include/kernel/multiboot.h
@@ -244,6 +244,22 @@ struct multiboot_mmap_entry
 } __attribute__((packed));
 typedef struct multiboot_mmap_entry multiboot_memory_map_t;
 
+struct multiboot_mmap_entry_64split   // has 64 bit fields split in two, otherwise compiler mixes all fields
+{
+  multiboot_uint32_t size;
+  multiboot_uint32_t addr_part1;
+  multiboot_uint32_t addr_part2;
+  multiboot_uint32_t len_part1;
+  multiboot_uint32_t len_part2;
+#define MULTIBOOT_MEMORY_AVAILABLE              1
+#define MULTIBOOT_MEMORY_RESERVED               2
+#define MULTIBOOT_MEMORY_ACPI_RECLAIMABLE       3
+#define MULTIBOOT_MEMORY_NVS                    4
+#define MULTIBOOT_MEMORY_BADRAM                 5
+  multiboot_uint32_t type;
+} __attribute__((packed));
+typedef struct multiboot_mmap_entry_64split multiboot_memory_map_64split_t;
+
 struct multiboot_mod_list
 {
   /* the memory used goes from bytes ’mod_start’ to ’mod_end-1’ inclusive */
