ae21a0c37f8f2b97483d4960a1b360d6f4bd2490
NGX123 <nikitja132@gmail.com>
2021-01-24 12:12:13 +0200

    added memory map entry structure; renamed kernInfo into bootInfo structure

--- src/include/kernel/init.h
+++ src/include/kernel/init.h
@@ -15,12 +15,31 @@
 #define PROTOCOL_NONE       0x0
 #define PROTOCOL_MULTIBOOT  0x01
 
+#define MEMINFO_RAM_AMOUNT  0x1
+#define MEMINFO_MEM_MAP     0x2
 
-struct kernInfo
+
+struct bootInfo
 {
     uint16_t protocol;
     void* boot_protocol_struct_ptr;
     void* boot_protocol_struct_end_ptr;
+};
+
+struct kernelMemMap
+{
+    void* mem_area_start_addr;
+    void* mem_area_end_addr;
+    size_t mem_area_size;
+    uint8_t mem_area_type;
+};
+
+struct memInfo
+{
+    uint8_t flags;
+    uint64_t lower_ram_size;
+    uint64_t upper_ram_size;
+    struct kernelMemMap memory_map;
 };
 
 
