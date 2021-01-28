9fe5d632c93296b46dcdb6bdf0f531d3a4c1aea0
NGX123 <nikitja132@gmail.com>
2021-01-28 08:18:23 +0200

    fixed booting specifing disk to use

--- src/arch/x86-64/init.s
+++ src/arch/x86-64/init.s
@@ -2,17 +2,44 @@
 ; @brief = code for initial setup of the 64 bit UEFI Kernel
 
 
+global _start
+
+
 MAGIC       equ 0xE85250D6
 ARCH_x86_32 equ 0
-HEADER_LEN  equ      24
-CHECKSUM    equ      -(MAGIC + ARCH_x86_32 + HEADER_LEN + 8)
+HEADER_LEN  equ 24
+CHECKSUM    equ -(MAGIC + ARCH_x86_32 + HEADER_LEN + 8)
 
 
 section .multiboot
-align    8
-    dd   MAGIC
-    dd   ARCH_x86_32
-    dd   HEADER_LEN
-    dd   CHECKSUM
-    dd   0            ;tag end
-    dd   8
+multiboot_header_start:
+    align    8
+        dd   MAGIC
+        dd   ARCH_x86_32
+        dd   multiboot_header_end - multiboot_header_start
+        dd   CHECKSUM
+multiboot_header_end:
+
+
+section .text
+_start:
+    hlt
+    jmp _start
+
+
+; MBALIGN         equ 1 << 0              ; Loaded modules page boundaries aligning flag
+; MEMINFO         equ 1 << 1              ; Memory map flag
+; FLAGS           equ MBALIGN | MEMINFO   ; GRUB boot flags combination
+; MAGIC           equ 0x1BADB002          ; Value for grub detection
+; CHECKSUM        equ -(MAGIC + FLAGS)   ; checksum of above, to prove we are multiboot
+
+; section .multiboot              ; GRUB section, includes writing special data at start of binary for grub to see the OS
+; align 4
+;     dd MAGIC
+;     dd FLAGS
+;     dd CHECKSUM
+
+; section .text
+; _start:
+;     hlt
+;     jmp _start
