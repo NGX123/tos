e8680362c857e77f8fdee2a6a72e9ac96a76cc1b
NGX123 <nikitja132@gmail.com>
2021-01-29 23:54:52 +0200

    removed the useless commented out code

--- src/arch/x86-64/init.s
+++ src/arch/x86-64/init.s
@@ -23,21 +23,3 @@ section .text
 _start:
     hlt
     jmp _start
-
-
-; MBALIGN         equ 1 << 0              ; Loaded modules page boundaries aligning flag
-; MEMINFO         equ 1 << 1              ; Memory map flag
-; FLAGS           equ MBALIGN | MEMINFO   ; GRUB boot flags combination
-; MAGIC           equ 0x1BADB002          ; Value for grub detection
-; CHECKSUM        equ -(MAGIC + FLAGS)   ; checksum of above, to prove we are multiboot
-
-; section .multiboot              ; GRUB section, includes writing special data at start of binary for grub to see the OS
-; align 4
-;     dd MAGIC
-;     dd FLAGS
-;     dd CHECKSUM
-
-; section .text
-; _start:
-;     hlt
-;     jmp _start
