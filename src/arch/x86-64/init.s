722c5d5c4a72dd07bed166244f1e4af625bd8874
NGX123 <nikitja132@gmail.com>
2021-01-30 09:58:01 +0200

    removed seperate section for multiboot header and moved it info the .text section; added comments

--- src/arch/x86-64/init.s
+++ src/arch/x86-64/init.s
@@ -5,21 +5,21 @@
 global _start
 
 
-MAGIC       equ 0xE85250D6
-ARCH_x86_32 equ 0
+; Kernel start called by grub(_start is specified in linker.ld)
+MAGIC       equ 0xE85250D6  ; Magic number used by GRUB to detect the executable is multiboot2 compatible
+ARCH_x86_32 equ 0           ; Architecture that is used by executbale, 0 - i386
 
 
-section .multiboot
-multiboot_header_start:
+section .text
+multiboot_header_start: ; Writes 64-bit aligned data directly to the start of executble so grub can find the header
     align 8
         dd  MAGIC
         dd  ARCH_x86_32
         dd  multiboot_header_end - multiboot_header_start
-        dd  -(MAGIC + ARCH_x86_32 + (multiboot_header_end - multiboot_header_start) + 8)
+        dd  -(MAGIC + ARCH_x86_32 + (multiboot_header_end - multiboot_header_start))    ; Checksum
 multiboot_header_end:
 
 
-section .text
 _start:
     hlt
     jmp _start
