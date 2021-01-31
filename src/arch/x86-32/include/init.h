b8c63f37b3f717320e910e4ff528633eaca287d2
NGX123 <nikitja132@gmail.com>
2021-01-31 23:25:20 +0200

    removed everything releated to multiboot

--- src/arch/x86-32/include/init.h
+++ src/arch/x86-32/include/init.h
@@ -14,9 +14,6 @@
 #include <kernel/init.h>
 #include "string.h"
 
-#define TOGGLE_BIT_ON   1
-#define TOGGLE_BIT_OFF  0
-
 
 struct gdt_entry
 {
@@ -50,19 +47,4 @@ static void makeMemFlat();
     @brief = Sets up memory paging
 */
 // static void initPaging();
-
-/*
-    @brief = changes the chosen bit in supplied variable to chosen value(ON(1) or OFF(0))
-    @param var = pointer to variable where the changes will be made
-    @param bitmask = the bit which has to be changed
-    @param bit_status = to which status the bit should be changed
-*/
-static void toggleBit(size_t* var, size_t bitmask, uint8_t bit_status);
-
-/*
-    @brief = constantly returns filled memInfo structs with new entries from mem map
-    @param count = variable which contains the number of the entrys that should be read, sets error bit when entries list reaches end, variable should be increased by user
-    @return = filled memInfo struct
-*/
-static struct memInfo getMultibootMemInfo(int count);
 #endif
