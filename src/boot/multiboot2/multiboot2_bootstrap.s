90cdbc4ea7d4bf1150894cf7a4f4d7b983041a3f
NGX123 <nikitja132@gmail.com>
2021-02-01 17:30:47 +0200

    instead of uncommenting the multiboot1 header now can be used by defining a macro

--- src/boot/multiboot2/multiboot2_bootstrap.s
+++ src/boot/multiboot2/multiboot2_bootstrap.s
@@ -8,6 +8,7 @@ global _start
 extern kernel_main
 
 
+%ifndef USE_MULTIBOOT1_PROTOCOL
 ; If problems occur return to equ instead of %define
 %define MAGIC                           0xE85250D6      ; Magic number used by GRUB to detect the executable is multiboot2 compatible
 %define ARCH_x86_32                     0               ; Architecture that is used by executbale, 0 - i386
@@ -27,6 +28,22 @@ multiboot_header_start:
     dd 8                                                                            ; and size - 8
 multiboot_header_end:
 
+%elifdef USE_MULTIBOOT1_PROTOCOL
+
+MULTIBOOT 1 HEADER - may be used if something goes very wrong. Replace multiboot 2 header code with this
+MBALIGN         equ 1 << 0              ; Loaded modules page boundaries aligning flag
+MEMINFO         equ 1 << 1              ; Memory map flag
+FLAGS           equ MBALIGN | MEMINFO   ; GRUB boot flags combination
+MAGIC           equ 0x1BADB002          ; Value for grub detection
+CHECKSUM        equ -(MAGIC + FLAGS)   ; checksum of above, to prove we are multiboot
+
+
+section .multiboot              ; GRUB section, includes writing special data at start of binary for grub to see the OS
+align 4
+    dd MAGIC
+dd FLAGS
+dd CHECKSUM
+%endif
 
 section .bss
 align 16                        ; Aligning is required for stack
@@ -45,17 +62,3 @@ _start:
 hang:
         hlt
         jmp hang               ; Loop forever
-
-
-; MULTIBOOT 1 HEADER - may be used if something goes very wrong. Replace multiboot 2 header code with this
-; MBALIGN         equ 1 << 0              ; Loaded modules page boundaries aligning flag
-; MEMINFO         equ 1 << 1              ; Memory map flag
-; FLAGS           equ MBALIGN | MEMINFO   ; GRUB boot flags combination
-; MAGIC           equ 0x1BADB002          ; Value for grub detection
-; CHECKSUM        equ -(MAGIC + FLAGS)   ; checksum of above, to prove we are multiboot
-
-; section .multiboot              ; GRUB section, includes writing special data at start of binary for grub to see the OS
-; align 4
-;     dd MAGIC
-;     dd FLAGS
-;     dd CHECKSUM
