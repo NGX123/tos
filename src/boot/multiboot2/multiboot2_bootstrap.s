084eb1e80ef46786cc09d752057f8fcb8269229a
NGX123 <nikitja132@gmail.com>
2021-02-01 17:11:13 +0200

    changed equ to %define; moved the commented out multiboot 1 header to the end of the file

--- src/boot/multiboot2/multiboot2_bootstrap.s
+++ src/boot/multiboot2/multiboot2_bootstrap.s
@@ -8,23 +8,10 @@ global _start
 extern kernel_main
 
 
-; MULTIBOOT 1 HEADER - may be used if something goes very wrong
-; MBALIGN         equ 1 << 0              ; Loaded modules page boundaries aligning flag
-; MEMINFO         equ 1 << 1              ; Memory map flag
-; FLAGS           equ MBALIGN | MEMINFO   ; GRUB boot flags combination
-; MAGIC           equ 0x1BADB002          ; Value for grub detection
-; CHECKSUM        equ -(MAGIC + FLAGS)   ; checksum of above, to prove we are multiboot
-
-; section .multiboot              ; GRUB section, includes writing special data at start of binary for grub to see the OS
-; align 4
-;     dd MAGIC
-;     dd FLAGS
-;     dd CHECKSUM
-
-
-MAGIC                       equ 0xE85250D6  ; Magic number used by GRUB to detect the executable is multiboot2 compatible
-ARCH_x86_32                 equ 0           ; Architecture that is used by executbale, 0 - i386
-MULTIBOOT2_HEADER_TAG_END   equ 0           ; Indicates the end of tags section in multiboot2 header
+; If problems occur return to equ instead of %define
+%define MAGIC                           0xE85250D6      ; Magic number used by GRUB to detect the executable is multiboot2 compatible
+%define ARCH_x86_32                     0               ; Architecture that is used by executbale, 0 - i386
+%define MULTIBOOT2_HEADER_TAG_END       0               ; Indicates the end of tags section in multiboot2 header
 
 
 section .multiboot          ; Has data located that will be used by grub to find the executable
@@ -58,3 +45,17 @@ _start:
 hang:
         hlt
         jmp hang               ; Loop forever
+
+
+; MULTIBOOT 1 HEADER - may be used if something goes very wrong. Replace multiboot 2 header code with this
+; MBALIGN         equ 1 << 0              ; Loaded modules page boundaries aligning flag
+; MEMINFO         equ 1 << 1              ; Memory map flag
+; FLAGS           equ MBALIGN | MEMINFO   ; GRUB boot flags combination
+; MAGIC           equ 0x1BADB002          ; Value for grub detection
+; CHECKSUM        equ -(MAGIC + FLAGS)   ; checksum of above, to prove we are multiboot
+
+; section .multiboot              ; GRUB section, includes writing special data at start of binary for grub to see the OS
+; align 4
+;     dd MAGIC
+;     dd FLAGS
+;     dd CHECKSUM
