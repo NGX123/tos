6d54612914b3d8c2c99a6885b7ec05ddae840ac5
NGX123 <nikitja132@gmail.com>
2021-02-07 17:01:15 +0200

    split the io lib into .c and .h file; added the path to c file into x86_32 makefile

--- src/include/arch/io_x86.h
+++ src/include/arch/io_x86.h
@@ -1,6 +1,6 @@
 /*
     @author = ngx123
-    @brief = includes defines to use with the x86 CPUs
+    @brief = includes declarations for port IO on x86
 */
 
 
@@ -16,30 +16,21 @@
     @param port = the number of the port to output to
     @param data = the data to output to the port
 */
-static void outb(uint16_t port, uint8_t data)
-{
-    asm volatile ("outb %b0,%w1" : : "a"(data), "d"(port));
-}
+void outb(uint16_t port, uint8_t data);
 
 /*
     @breif = writes 16 bits to the port
     @param port = the number of port to write to
     @param data = the data to output to the port
 */
-static void outw(uint16_t port, uint16_t data)
-{
-  asm volatile ("outw %w0,%w1" : : "a"(data), "d"(port));
-}
+void outw(uint16_t port, uint16_t data);
 
 /*
     @breif = writes 32 bits to the port
     @param port = the number of port to write to
     @param data = the data to output to the port
 */
-static void outl(uint16_t port, uint32_t data)
-{
-    asm volatile ("outl %0,%w1" : : "a"(data), "d"(port));
-}
+void outl(uint16_t port, uint32_t data);
 
 
 /*
@@ -47,33 +38,19 @@ static void outl(uint16_t port, uint32_t data)
     @param port = the number of the port to read from
     @return = the data read from the port
 */
-static uint8_t inb(uint16_t port)
-{
-     uint8_t data;
-    asm __volatile ("inb %w1,%b0" : "=a"(data) : "d"(port));
-    return data;
-}
+uint8_t inb(uint16_t port);
 
 /*
     @brief = reads 16 bits from the port
     @param port = the number of the port to read from
     @return = the data read from the port
 */
-static uint16_t inw(uint16_t port)
-{
-    uint16_t data;
-    asm volatile ("inw %w1,%w0" : "=a"(data) : "d"(port));
-    return data;
-}
+uint16_t inw(uint16_t port);
 
 /*
     @brief = reads 32 bits from the port
     @param port = the number of the port to read from
     @return = the data read from the port
 */
-static uint32_t inl(uint16_t port) {
-    uint32_t data;
-    asm volatile ("inl %w1,%0" : "=a"(data) : "d"(port));
-    return data;
-}
+uint32_t inl(uint16_t port);
 #endif
