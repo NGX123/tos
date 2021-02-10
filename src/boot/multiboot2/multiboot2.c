7f28f75009eb6c16d8fc458ab27439996ed691b9
NGX123 <nikitja132@gmail.com>
2021-02-10 22:37:03 +0200

    removed the framebuffer tag parsing

--- src/boot/multiboot2/multiboot2.c
+++ src/boot/multiboot2/multiboot2.c
@@ -53,84 +53,9 @@ void interpretMultiboot2(uint32_t magic, uint32_t infoStruct_addr)
             //             printf (" base_addr = 0x%x%x," " length = 0x%x%x, type = 0x%x\n", (unsigned) (mmap->addr >> 32), (unsigned) (mmap->addr & 0xffffffff), (unsigned) (mmap->len >> 32), (unsigned) (mmap->len & 0xffffffff), (unsigned) mmap->type);
             //     }
             //     break;
-            case MULTIBOOT_TAG_TYPE_FRAMEBUFFER:
-            {
-                multiboot_uint32_t color;
-                unsigned i;
-                struct multiboot_tag_framebuffer *tagfb = (struct multiboot_tag_framebuffer *) tag_current;
-                void *fb = (void *) (unsigned long) tagfb->common.framebuffer_addr;
-
-                switch (tagfb->common.framebuffer_type) // Does setup based on the framebuffer type
-                {
-                    case MULTIBOOT_FRAMEBUFFER_TYPE_INDEXED:
-                        {
-                            unsigned best_distance, distance;
-                            struct multiboot_color *palette;
-
-                            palette = tagfb->framebuffer_palette;
-
-                            color = 0;
-                            best_distance = 4*256*256;
-
-                            for (i = 0; i < tagfb->framebuffer_palette_num_colors; i++)
-                            {
-                                distance = (0xff - palette[i].blue) * (0xff - palette[i].blue) + palette[i].red * palette[i].red + palette[i].green * palette[i].green;
-                                if (distance < best_distance)
-                                {
-                                    color = i;
-                                    best_distance = distance;
-                                }
-                            }
-                        }
-                        break;
-                    case MULTIBOOT_FRAMEBUFFER_TYPE_RGB:
-                        color = ((1 << tagfb->framebuffer_blue_mask_size) - 1) << tagfb->framebuffer_blue_field_position;
-                        break;
-                    case MULTIBOOT_FRAMEBUFFER_TYPE_EGA_TEXT:
-                        color = '\\' | 0x0100;
-                        break;
-                    default:
-                        color = 0xffffffff;
-                        break;
-                }
-
-                for (i = 0; i < tagfb->common.framebuffer_width && i < tagfb->common.framebuffer_height; i++)
-                {
-                    switch (tagfb->common.framebuffer_bpp)
-                    {
-                        case 8:
-                            {
-                                multiboot_uint8_t *pixel = fb + tagfb->common.framebuffer_pitch * i + i;
-                                *pixel = color;
-                            }
-                            break;
-                        case 15:
-                        case 16:
-                            {
-                                multiboot_uint16_t *pixel = fb + tagfb->common.framebuffer_pitch * i + 2 * i;
-                                *pixel = color;
-                            }
-                            break;
-                        case 24:
-                            {
-                                multiboot_uint32_t *pixel = fb + tagfb->common.framebuffer_pitch * i + 3 * i;
-                                *pixel = (color & 0xffffff) | (*pixel & 0xff000000);
-                            }
-                            break;
-
-                        case 32:
-                            {
-                                multiboot_uint32_t *pixel = fb + tagfb->common.framebuffer_pitch * i + 4 * i;
-                                *pixel = color;
-                            }
-                            break;
-                    }
-                }
+            case MULTIBOOT_TAG_TYPE_FRAMEBUFFER: /* MAKE IT DO THE NEEDED SETUP AND SET A BIT/VARIABLE TO INDICATE THAT FRAMEBUFFER WAS SETUP AND THE DRIVER WILL NOT WASTE TIME SEARCHING FOR IT */
                 break;
-            }
         }
-        tag_current = (struct multiboot_tag *)((multiboot_uint8_t *)tag_current + ((tag_current->size + 7) & ~7));
-        //printf ("Total mbi size 0x%x\n", (unsigned) tag - addr);
     }
 }
 
