5aa5ad57403b305e82faafb2e13431c75a4d9ac8
NGX123 <nikitja132@gmail.com>
2021-02-11 19:31:44 +0200

    fixed the serial output formatting; removed useless functions; added printf for debugging

--- src/boot/multiboot2/multiboot2.c
+++ src/boot/multiboot2/multiboot2.c
@@ -6,14 +6,13 @@
 #include "../../drivers/include/serial.h"
 
 void *multiboot_struct_start_ptr, *multiboot_struct_end_ptr;
-
-void printf(char *str);
+uint16_t avilability_flags;
 
 
 void interpretMultiboot2(uint32_t magic, uint32_t infoStruct_addr)
 {
     initSerial();
-    printSerial("---------------------------------------------------------------------------------------");
+    printSerial("---------------------------------------------------------------------------------------\n");
 
 
     unsigned int size;
@@ -21,46 +20,37 @@ void interpretMultiboot2(uint32_t magic, uint32_t infoStruct_addr)
 
     if (magic != MULTIBOOT2_BOOTLOADER_MAGIC)   // addr & 7, checks if the address is divisible 7, if yes - it fails becuase it means it is not aligned
     {
-        printf("---Magic number check failed---");
+        printf("Magic number check failed\n");
         return;
     }
     if (infoStruct_addr & 7)
     {
-        printf("---Align check failed---");
+        printf("Align check failed\n");
         return;
     }
 
     size = *(unsigned int*)infoStruct_addr;
     tag_start = (struct multiboot_tag *) (infoStruct_addr + 8);
-    // printf ("Announced mbi size 0x%x\n", size);
 
     for (tag_current = tag_start; tag_current->type != MULTIBOOT_TAG_TYPE_END; tag_current = (struct multiboot_tag *)((multiboot_uint8_t *)tag_current + ((tag_current->size + 7) & ~7)))
     {
-        //printf ("Tag 0x%x, Size 0x%x\n", tag->type, tag->size);
         switch (tag_current->type)
         {
-            case MULTIBOOT_TAG_TYPE_CMDLINE:        /* LOOK AT WHAT THE STRING CONTAINS AND IF IT IS NOTHING USEFULL REMOVE THIS TAG PARSING */
-                //printf ("Command line = %s\n", ((struct multiboot_tag_string *)tag_current)->string);
+            case MULTIBOOT_TAG_TYPE_BASIC_MEMINFO:
+                toggleBit((size_t*)&avilability_flags, AVAILABLE_FLAG_MEMSIZE, TOGGLE_BIT_ON);
                 break;
-            case MULTIBOOT_TAG_TYPE_BASIC_MEMINFO:  /* RECORD THE DATA INTO VARIABLES AND THEN getMemInfo WOULD NOT NEED TO SEARCH THE TAG EACH TIME AND COULD JUST READ INFO FROM VARIABLES */
-                //printf ("mem_lower = %uKB, mem_upper = %uKB\n", ((struct multiboot_tag_basic_meminfo *) tag)->mem_lower, ((struct multiboot_tag_basic_meminfo *) tag)->mem_upper);
-                break;
-            // case MULTIBOOT_TAG_TYPE_MMAP:           /* DO NOT INTERPRET, INSTEAD MARK A FLAG OR A VARIABLE TO SHOW THAT MEMORY MAP IS AVIALABLE AND THEN IF getMemInfo() will see that there is no memory map it will fail, otherwise interpret it */
-            //     {
-            //         multiboot_memory_map_t *mmap;
+            case MULTIBOOT_TAG_TYPE_MMAP:           /* DO NOT INTERPRET, INSTEAD MARK A FLAG OR A VARIABLE TO SHOW THAT MEMORY MAP IS AVIALABLE AND THEN IF getMemInfo() will see that there is no memory map it will fail, otherwise interpret it */
+                {
+                    multiboot_memory_map_t *mmap;
 
-            //         for (mmap = ((struct multiboot_tag_mmap *)tag_current)->entries; (multiboot_uint8_t *) mmap < (multiboot_uint8_t *)tag_current + tag_current->size; mmap = (multiboot_memory_map_t *)((unsigned long) mmap + ((struct multiboot_tag_mmap *) tag_current)->entry_size))
-            //             printf (" base_addr = 0x%x%x," " length = 0x%x%x, type = 0x%x\n", (unsigned) (mmap->addr >> 32), (unsigned) (mmap->addr & 0xffffffff), (unsigned) (mmap->len >> 32), (unsigned) (mmap->len & 0xffffffff), (unsigned) mmap->type);
-            //     }
-            //     break;
+                    for (mmap = ((struct multiboot_tag_mmap *)tag_current)->entries; (multiboot_uint8_t *) mmap < (multiboot_uint8_t *)tag_current + tag_current->size; mmap = (multiboot_memory_map_t *)((unsigned long) mmap + ((struct multiboot_tag_mmap *) tag_current)->entry_size))
+                        printf (" base_addr = 0x%x%x," " length = 0x%x%x, type = 0x%x\n", (unsigned) (mmap->addr >> 32), (unsigned) (mmap->addr & 0xffffffff), (unsigned) (mmap->len >> 32), (unsigned) (mmap->len & 0xffffffff), (unsigned) mmap->type);
+                }
+                break;
             case MULTIBOOT_TAG_TYPE_FRAMEBUFFER: /* MAKE IT DO THE NEEDED SETUP AND SET A BIT/VARIABLE TO INDICATE THAT FRAMEBUFFER WAS SETUP AND THE DRIVER WILL NOT WASTE TIME SEARCHING FOR IT */
                 break;
         }
     }
-}
-
-void bootloaderInterface()  /* For special reqests for information from the bootloader */
-{
 }
 
 static void toggleBit(size_t* var, size_t bitmask, uint8_t bit_status)
@@ -74,8 +64,99 @@ static void toggleBit(size_t* var, size_t bitmask, uint8_t bit_status)
 
 
 
-
-void printf(char *str)
+/* TOMPORARY IMPLEMENTATION FOR DEBUGGING */
+int putchar(int chara)
 {
-    printSerial(str);
+    writeSerial(chara);
+    return 0;
+}
+
+static void printint(int xx, int base, int sgn)
+{
+    static char digits[] = "0123456789ABCDEF";
+    char buf[16];
+    int i, neg;
+    uint32_t x;
+
+    neg = 0;
+    if(sgn && xx < 0){
+        neg = 1;
+        x = -xx;
+    }
+    else
+        x = xx;
+
+    i = 0;
+    do
+    {
+        buf[i++] = digits[x % base];
+    } while((x /= base) != 0);
+
+    if (neg)
+        buf[i++] = '-';
+
+    while(--i >= 0)
+        putchar(buf[i]);
+}
+
+void printf(const char *fmt, ...) // only understands %d, %x, %p, %s, %c
+{
+  char *s;
+  int c, i, state;
+  uint32_t *ap;
+
+  state = 0;
+  ap = (uint32_t*)(void*)&fmt + 1;
+  for(i = 0; fmt[i]; i++)
+  {
+    c = fmt[i] & 0xff;
+    if(state == 0)
+    {
+        if(c == '%')
+            state = '%';
+        else
+            putchar(c);
+    }
+    else if(state == '%')
+    {
+        if(c == 'd')
+        {
+            printint((int)*ap, 10, 1);
+            ap++;
+        }
+        else if(c == 'x' || c == 'p')
+        {
+            printint((int)*ap, 16, 0);
+            ap++;
+        }
+        else if(c == 's')
+        {
+            s = (char*)*ap;
+            ap++;
+            if(s == 0)
+                s = "(null)";
+            while(*s != 0)
+            {
+                putchar(*s);
+                s++;
+            }
+        }
+        else if(c == 'c')
+        {
+            putchar((int)*ap);
+            ap++;
+        }
+        else if(c == '%')
+        {
+            putchar(c);
+        }
+        else
+        {
+            // Unknown % sequence.  Print it to draw attention.
+            putchar('%');
+            putchar(c);
+        }
+        state = 0;
+    }
+  }
 }
