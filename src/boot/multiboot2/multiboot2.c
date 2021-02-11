c842ccc5957a866d1ba95f6446fc024424157fc3
NGX123 <nikitja132@gmail.com>
2021-02-11 20:02:48 +0200

    added recording of start and end addresses; added the recording of bits to tell which tags are present; fixed the serial output formatting

--- src/boot/multiboot2/multiboot2.c
+++ src/boot/multiboot2/multiboot2.c
@@ -5,17 +5,17 @@
 #include "multiboot-include.h"
 #include "../../drivers/include/serial.h"
 
-void *multiboot_struct_start_ptr, *multiboot_struct_end_ptr;
-uint16_t avilability_flags;
+static void *multiboot_struct_start_ptr, *multiboot_struct_end_ptr;
+static uint16_t avilability_flags;
 
 
 void interpretMultiboot2(uint32_t magic, uint32_t infoStruct_addr)
 {
     initSerial();
-    printSerial("---------------------------------------------------------------------------------------\n");
+    printSerial("\n\n---------------------------------------------------------------------------------------\n");
 
 
-    unsigned int size;
+    uint32_t all_tags_size;
     struct multiboot_tag *tag_start, *tag_current;
 
     if (magic != MULTIBOOT2_BOOTLOADER_MAGIC)   // addr & 7, checks if the address is divisible 7, if yes - it fails becuase it means it is not aligned
@@ -29,8 +29,11 @@ void interpretMultiboot2(uint32_t magic, uint32_t infoStruct_addr)
         return;
     }
 
-    size = *(unsigned int*)infoStruct_addr;
+    all_tags_size = *(uint32_t*)infoStruct_addr;
     tag_start = (struct multiboot_tag *) (infoStruct_addr + 8);
+
+    multiboot_struct_start_ptr = (void*)infoStruct_addr;
+    multiboot_struct_end_ptr = (void*)(infoStruct_addr + all_tags_size - 1);
 
     for (tag_current = tag_start; tag_current->type != MULTIBOOT_TAG_TYPE_END; tag_current = (struct multiboot_tag *)((multiboot_uint8_t *)tag_current + ((tag_current->size + 7) & ~7)))
     {
@@ -39,15 +42,11 @@ void interpretMultiboot2(uint32_t magic, uint32_t infoStruct_addr)
             case MULTIBOOT_TAG_TYPE_BASIC_MEMINFO:
                 toggleBit((size_t*)&avilability_flags, AVAILABLE_FLAG_MEMSIZE, TOGGLE_BIT_ON);
                 break;
-            case MULTIBOOT_TAG_TYPE_MMAP:           /* DO NOT INTERPRET, INSTEAD MARK A FLAG OR A VARIABLE TO SHOW THAT MEMORY MAP IS AVIALABLE AND THEN IF getMemInfo() will see that there is no memory map it will fail, otherwise interpret it */
-                {
-                    multiboot_memory_map_t *mmap;
-
-                    for (mmap = ((struct multiboot_tag_mmap *)tag_current)->entries; (multiboot_uint8_t *) mmap < (multiboot_uint8_t *)tag_current + tag_current->size; mmap = (multiboot_memory_map_t *)((unsigned long) mmap + ((struct multiboot_tag_mmap *) tag_current)->entry_size))
-                        printf (" base_addr = 0x%x%x," " length = 0x%x%x, type = 0x%x\n", (unsigned) (mmap->addr >> 32), (unsigned) (mmap->addr & 0xffffffff), (unsigned) (mmap->len >> 32), (unsigned) (mmap->len & 0xffffffff), (unsigned) mmap->type);
-                }
+            case MULTIBOOT_TAG_TYPE_MMAP:
+                toggleBit((size_t*)&avilability_flags, AVAILABLE_FLAG_MEMMAP, TOGGLE_BIT_ON);
                 break;
-            case MULTIBOOT_TAG_TYPE_FRAMEBUFFER: /* MAKE IT DO THE NEEDED SETUP AND SET A BIT/VARIABLE TO INDICATE THAT FRAMEBUFFER WAS SETUP AND THE DRIVER WILL NOT WASTE TIME SEARCHING FOR IT */
+            case MULTIBOOT_TAG_TYPE_FRAMEBUFFER:
+                toggleBit((size_t*)&avilability_flags, AVAILABLE_FLAG_FRAMEBUFFER, TOGGLE_BIT_ON);
                 break;
         }
     }
