28406454662ca4e682c476fa899e05113dec9f15
NGX123 <nikitja132@gmail.com>
2021-02-19 18:50:02 +0200

    reduced all zeroing to one 0; changed formatting; changed comments; added comments on translation of hex to binary

--- src/arch/x86-32/init.c
+++ src/arch/x86-32/init.c
@@ -13,29 +13,29 @@ static struct GDTdescriptor gdt32_ptr;
 
 void makeMemFlat()
 {
-    // Null Descriptor
-    gdt32[0].limit_low = 0x0000;
-    gdt32[0].base_low = 0x0000;
-    gdt32[0].base_middle = 0x00;
-    gdt32[0].access = 0x00;
-    gdt32[0].granularity = 0x00;
-    gdt32[0].base_high = 0x00;
+    // Null Selector
+    gdt32[0].limit_low      = 0;
+    gdt32[0].base_low       = 0;
+    gdt32[0].base_middle    = 0;
+    gdt32[0].access         = 0;
+    gdt32[0].granularity    = 0;
+    gdt32[0].base_high      = 0;
 
-    // Code Descriptor
-    gdt32[1].limit_low = 0xffff;
-    gdt32[1].base_low = 0x0000;
-    gdt32[1].base_middle = 0x00;
-    gdt32[1].access = 0x9A;
-    gdt32[1].granularity = 0xCF;
-    gdt32[1].base_high = 0x00;
+    // Code Selector
+    gdt32[1].limit_low      = 0xffff;
+    gdt32[1].base_low       = 0;
+    gdt32[1].base_middle    = 0;
+    gdt32[1].access         = 0x9A; // 10011010
+    gdt32[1].granularity    = 0xCF; // 11001111
+    gdt32[1].base_high      = 0;
 
-    // Data Descriptor
-    gdt32[2].limit_low = 0xffff;
-    gdt32[2].base_low = 0x0000;
-    gdt32[2].base_middle = 0x00;
-    gdt32[2].access = 0x92;
-    gdt32[2].granularity = 0xCF;
-    gdt32[2].base_high = 0x00;
+    // Data Selector
+    gdt32[2].limit_low      = 0xffff;
+    gdt32[2].base_low       = 0;
+    gdt32[2].base_middle    = 0;
+    gdt32[2].access         = 0x92; // 10010010
+    gdt32[2].granularity    = 0xCF; // 11001111
+    gdt32[2].base_high      = 0;
 
     // GDT Descriptor
     gdt32_ptr.limit = sizeof(gdt32) - 1;
