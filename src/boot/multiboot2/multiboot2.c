e6c73d5b3243205987491cbde9791bdf7929912e
NGX123 <nikitja132@gmail.com>
2021-02-26 11:08:54 +0200

    cleaned out the file; added comments; implemented bootloader interface command

--- src/boot/multiboot2/multiboot2.c
+++ src/boot/multiboot2/multiboot2.c
@@ -7,39 +7,55 @@
 #include "../../drivers/include/serial.h"
 #include "stdio.h"
 
-static void *multiboot_struct_start_ptr, *multiboot_struct_end_ptr;
+
+extern uint64_t multiboot_magic_var;
+extern uint64_t multiboot_tags_address_var;
+
+static unsigned long multiboot2_magic_copy;				// Space to store multiboot magic number locally so it is not stored in some random .bss section and can be lost
+static address_tt multiboot2_tags_address_copy;			// Same but for tags address
+
+static unsigned long multiboot2_all_tags_size;			// Tags structures toatal size variable, for use in mmap(to mark the space where the tags exist as reserved in the page frame allocator)
+
 static uint16_t avilability_flags;
 
 
-void interpretMultiboot2(uint32_t magic, uint32_t infoStruct_addr)
+void arch_bootloaderInterface(uint32_t function)
 {
+	if (function == BOOTLOADER_FUNCTION_INIT)
+		interpretMultiboot2();
+}
+
+void interpretMultiboot2(void)
+{
+	/* REMOVE THIS */
     initSerial();
     printSerial("\n\n---------------------------------------------------------------------------------------\n");
+	/*             */
 
 
-    uint32_t all_tags_size;
-    struct multiboot_tag *tag_start, *tag_current;
 
-    if (magic != MULTIBOOT2_BOOTLOADER_MAGIC)   // addr & 7, checks if the address is divisible 7, if yes - it fails becuase it means it is not aligned
+    struct multiboot_tag *multiboot2_tags_start_address_converted, *multiboot2_tag_current;
+
+    if (multiboot_magic_var != MULTIBOOT2_BOOTLOADER_MAGIC)
     {
         printf("Magic number check failed\n");
         return;
     }
-    if (infoStruct_addr & 7)
+    if (multiboot_tags_address_var & 7)																// addr & 7, checks if the address is divisible 7, if yes - it fails becuase it means it is not aligned
     {
         printf("Align check failed\n");
         return;
     }
 
-    all_tags_size = *(uint32_t*)infoStruct_addr;
-    tag_start = (struct multiboot_tag *) (infoStruct_addr + 8);
+	multiboot2_magic_copy = multiboot_magic_var;													/* COPY THE MAGIC number and TAGS ptr to local variables */
+	multiboot2_tags_address_copy = multiboot_tags_address_var;
 
-    multiboot_struct_start_ptr = (void*)infoStruct_addr;
-    multiboot_struct_end_ptr = (void*)(infoStruct_addr + all_tags_size - 1);
+    multiboot2_all_tags_size = *(uint32_t*)multiboot_tags_address_var;								// First in tags structure the size is located in uint_32 format so to get it the address is just converted to int32 pointer
+    multiboot2_tags_start_address_converted = (struct multiboot_tag*)(multiboot_tags_address_var + 8); 	// The multiboot2 tags pointer points not directly to flags but to a structure where first field is size, so the address has to be converted to point to tags
 
-    for (tag_current = tag_start; tag_current->type != MULTIBOOT_TAG_TYPE_END; tag_current = (struct multiboot_tag *)((multiboot_uint8_t *)tag_current + ((tag_current->size + 7) & ~7)))
+    for (multiboot2_tag_current = multiboot2_tags_start_address_converted; multiboot2_tag_current->type != MULTIBOOT_TAG_TYPE_END; multiboot2_tag_current = (struct multiboot_tag*)((multiboot_uint8_t*)multiboot2_tag_current + ((multiboot2_tag_current->size + 7) & ~7)))	// Loop through all tags and do the needed thing for each tag
     {
-        switch (tag_current->type)
+        switch (multiboot2_tag_current->type)
         {
             case MULTIBOOT_TAG_TYPE_MMAP:
                 toggleBit((size_t*)&avilability_flags, AVAILABLE_FLAG_MEMMAP, TOGGLE_BIT_ON);
@@ -57,23 +73,23 @@ void interpretMultiboot2(uint32_t magic, uint32_t infoStruct_addr)
 
     for (i = 0; ((memory_map = arch_getMemInfo(i)).flags & MEMINFO_FLAG_ERROR) == 0; i++)
     {
-        printf("addr = 0x%x%x, length = 0x%x%x, type = 0x%x\n", memory_map.addr_part2, memory_map.addr_part1, memory_map.size_part2, memory_map.size_part1, memory_map.type);
+        printf("addr = 0x%lx, length = 0x%lx, type = 0x%x\n", memory_map.start_address, memory_map.area_size, memory_map.area_type);
     }
 }
 
 struct memInfo arch_getMemInfo(int count)
 {
     struct memInfo returnStruct = {0};
-    struct multiboot_tag *tag_current, *tag_start = (struct multiboot_tag *) (multiboot_struct_start_ptr + 8);
+    struct multiboot_tag *tag_current, *tag_start = (struct multiboot_tag *) (multiboot_tags_address_var + 8);
     multiboot_memory_map_t *mmap;
 
-    if (!(avilability_flags & AVAILABLE_FLAG_MEMMAP))
+    if (!(avilability_flags & AVAILABLE_FLAG_MEMMAP))	// Return error if the bootloader hasn't given the memory map
     {
         toggleBit((size_t*)&returnStruct.flags, MEMINFO_FLAG_ERROR, TOGGLE_BIT_ON);
         return returnStruct;
     }
 
-    for (tag_current = tag_start; tag_current->type != MULTIBOOT_TAG_TYPE_END; tag_current = (struct multiboot_tag *)((multiboot_uint8_t *)tag_current + ((tag_current->size + 7) & ~7)))
+    for (tag_current = tag_start; tag_current->type != MULTIBOOT_TAG_TYPE_END; tag_current = (struct multiboot_tag *)((multiboot_uint8_t *)tag_current + ((tag_current->size + 7) & ~7)))	// Parse the memory map and put it in the kernels format
         if (tag_current->type == MULTIBOOT_TAG_TYPE_MMAP)
         {
             mmap = ((struct multiboot_tag_mmap *) tag_current)->entries;
@@ -81,18 +97,9 @@ struct memInfo arch_getMemInfo(int count)
 
             if ((multiboot_uint8_t *) mmap < (multiboot_uint8_t *) tag_current + tag_current->size)
             {
-                returnStruct.addr_full = mmap->addr;
-                returnStruct.size_full = mmap->len;
-                returnStruct.type = mmap->type;
-
-                /* FIX: if problems occur split the 64bit fields into 32bit fields */
-                // uint32_t *mmap_addr32, *mmap_len32;
-                // mmap_addr32 = (uint32_t*)&mmap->addr;
-                // mmap_len32 = (uint32_t*)&mmap->len;
-                // returnStruct.addr1 = *mmap_addr32;
-                // returnStruct.addr2 = *(mmap_addr32+1);
-                // returnStruct.size1 = *mmap_len32;
-                // returnStruct.size2 = *(mmap_len32+1);
+                returnStruct.start_address = mmap->addr;
+                returnStruct.area_size = mmap->len;
+                returnStruct.area_type = mmap->type;
             }
             else
             {
@@ -129,110 +136,4 @@ int putchar(int chara)
 {
     writeSerial(chara);
     return 0;
-}
-
-static void printint(int xx, int base, int sgn)
-{
-    static char digits[] = "0123456789ABCDEF";
-    char buf[16];
-    int i, neg;
-    uint32_t x;
-
-    neg = 0;
-    if(sgn && xx < 0){
-        neg = 1;
-        x = -xx;
-    }
-    else
-        x = xx;
-
-    i = 0;
-    do
-    {
-        buf[i++] = digits[x % base];
-    } while((x /= base) != 0);
-
-    if (neg)
-        buf[i++] = '-';
-
-    while(--i >= 0)
-        putchar(buf[i]);
-}
-
-void printf(const char *fmt, ...) // only understands %d, %x, %p, %s, %c
-{
-  char *s;
-  int c, i, state;
-  uint32_t *ap;
-
-  state = 0;
-  ap = (uint32_t*)(void*)&fmt + 1;
-  for(i = 0; fmt[i]; i++)
-  {
-    c = fmt[i] & 0xff;
-    if(state == 0)
-    {
-        if(c == '%')
-            state = '%';
-        else
-            putchar(c);
-    }
-    else if(state == '%')
-    {
-        if(c == 'd')
-        {
-            printint((int)*ap, 10, 1);
-            ap++;
-        }
-        else if(c == 'x' || c == 'p')
-        {
-            printint((int)*ap, 16, 0);
-            ap++;
-        }
-        else if(c == 's')
-        {
-            s = (char*)*ap;
-            ap++;
-            if(s == 0)
-                s = "(null)";
-            while(*s != 0)
-            {
-                putchar(*s);
-                s++;
-            }
-        }
-        else if(c == 'c')
-        {
-            putchar((int)*ap);
-            ap++;
-        }
-        else if(c == '%')
-        {
-            putchar(c);
-        }
-        else
-        {
-            // Unknown % sequence.  Print it to draw attention.
-            putchar('%');
-            putchar(c);
-        }
-        state = 0;
-    }
-  }
-}
-
-void* memcpy(void *dest, const void *src, size_t n) {	/*CUSTOM*/
-    char *dp = dest;
-    const char *sp = src;
-    while (n--)
-        *dp++ = *sp++;
-    return dest;
-}
-
-void *memset(void *s, int c, size_t n)	/*CUSTOM*/
-{
-    unsigned char* p=s;
-    while(n--)
-        *p++ = (unsigned char)c;
-    return s;
 }
