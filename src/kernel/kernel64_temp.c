aa3d4aa63bc41bfde1a26c10becb722a2edb8d88
NGX123 <nikitja132@gmail.com>
2021-03-02 10:05:20 +0200

    1. Added more defines 2. Added better description for getMemInfo 3. Added better description for bootloaderInterface

--- src/kernel/kernel64_temp.c
+++ src/kernel/kernel64_temp.c
@@ -12,6 +12,29 @@ void kernel_main()
 	// Call the bootloader function to initialize whatever it needs
 	arch_bootloaderInterface(BOOTLOADER_FUNCTION_INIT);
 
+
+	/* DEBUG CODE */
+	initSerial();
+	printSerial("\n\n---------------------------------------------------------------------------------------\n");
+
+	printf("Hello");
+
+	struct memInfo memory_map;
+	int i;
+
+	for (i = 0; ((memory_map = arch_getMemInfo(i, MEMMAP_TYPE_PROTOCOL)).flags & MEMINFO_FLAG_ERROR) == 0; i++)
+	{
+		printf("addr = 0x%lx, length = 0x%lx, type = 0x%x\n", memory_map.start_address, memory_map.area_size, memory_map.area_type);
+	}
+
+	printf("MemMap by Interprter\n");
+
+	for (i = 0; ((memory_map = arch_getMemInfo(i, MEMMAP_TYPE_INTERPRETER)).flags & MEMINFO_FLAG_ERROR) == 0; i++)
+	{
+		printf("addr = 0x%lx, length = 0x%lx, type = 0x%x\n", memory_map.start_address, memory_map.area_size, memory_map.area_type);
+	}
+
+
 	while(1)
 		;
 }
