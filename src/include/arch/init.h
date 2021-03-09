6a4859681690fe7ae6a5e3295307649970261dfa
NGX123 <nikitja132@gmail.com>
2021-03-09 17:24:00 +0200

    1. Changed memory map section name defentions 2. Added ability to return data by bootloaderInterface

--- src/include/arch/init.h
+++ src/include/arch/init.h
@@ -15,8 +15,7 @@
 
 #define MEMMAP_AREA_TYPE_USABLE     0x1
 #define MEMMAP_AREA_TYPE_RESERVED   0x2
-#define MEMMAP_AREA_TYPE_SPECIAL    0x3 // Area that was reserved by kernel
-#define MEMMAP_AREA_TYPE_OTHER      0x4 // Bootloader should be asked about this memory using bootloaderInterface()
+#define MEMMAP_AREA_TYPE_KERNEL		0x3
 
 #define MEMMAP_TYPE_PROTOCOL		0x0	// Make getmeminfo return the memory map supplied by bootloader protocol
 #define MEMMAP_TYPE_INTERPRETER		0x1	// Make getmeminfo return the memory map additional fields created by the boot protocol interpreter(e.g. multiboot2.c)
@@ -52,7 +51,7 @@ extern struct memInfo arch_getMemInfo(size_t count, uint8_t mmap_type);
 	@param data = unspecified pointer(void*) to some data returned by function(can point to nothing)
 	@return = 0 on success, -1 on fail
 */
-extern int arch_bootloaderInterface(uint32_t function);
+extern int arch_bootloaderInterface(uint32_t function, void* data);
 
 /*
     @brief = main function of the kernel that calls all other functions
